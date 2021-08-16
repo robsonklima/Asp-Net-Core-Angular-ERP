0e1edb7a908350b2c70f3a53e967a303537a3b31
robson <robsonklima@gmail.com>
2021-08-16 20:33:41 -0300

    REF

--- SAT.FRONT/src/app/modules/main/default/default.component.ts
+++ SAT.FRONT/src/app/modules/main/default/default.component.ts
@@ -1,11 +1,22 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { UsuarioSessionData } from 'app/core/types/usuario.types';
+import { UserService } from 'app/core/user/user.service';
 
 @Component({
     selector     : 'default',
     templateUrl  : './default.component.html',
     encapsulation: ViewEncapsulation.None
 })
-export class DefaultComponent
-{
-    constructor() {}
+export class DefaultComponent {
+    sessionData: UsuarioSessionData;
+
+    constructor(
+        private _userService: UserService
+    ) {
+        this.sessionData = JSON.parse(this._userService.userSession);
+    }
+
+    ngOnInit() {
+        
+    }
 }

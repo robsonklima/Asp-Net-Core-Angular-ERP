e3b933c5e9468cf237b5f6cae7303a4f0834b270
robson <robsonklima@gmail.com>
2021-08-17 23:50:19 -0300

    Layout: Relatorio Atendimento Form

--- SAT.FRONT/src/app/modules/main/relatorio-atendimento/relatorio-atendimento-detalhe-form/relatorio-atendimento-detalhe-form.component.ts
+++ SAT.FRONT/src/app/modules/main/relatorio-atendimento/relatorio-atendimento-detalhe-form/relatorio-atendimento-detalhe-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { TipoServicoService } from 'app/core/services/tipo-servico.service';
 import { CausaService } from 'app/core/services/causa.service';
 import { TipoServico, TipoServicoData } from 'app/core/types/tipo-servico.types';
+import { RelatorioAtendimentoFormComponent } from '../relatorio-atendimento-form/relatorio-atendimento-form.component';
 import { Causa, CausaData } from 'app/core/types/causa.types';
 import { DefeitoService } from 'app/core/services/defeito.service';
 import { AcaoService } from 'app/core/services/acao.service';
@@ -14,7 +15,6 @@ import { Usuario } from 'app/core/types/usuario.types';
 import { UserService } from 'app/core/user/user.service';
 import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
-import { RelatorioAtendimentoFormComponent } from '../relatorio-atendimento-form/relatorio-atendimento-form.component';
 import { MatDialogRef } from '@angular/material/dialog';
 import moment from 'moment';
 
@@ -35,9 +35,7 @@ export class RelatorioAtendimentoDetalheFormComponent implements OnInit, OnDestr
   tipoCausa: TipoCausa;
   usuario: Usuario;
   tipoServicoFilterCtrl: FormControl = new FormControl();
-  moduloFilterCtrl: FormControl = new FormControl();
-  subModuloFilterCtrl: FormControl = new FormControl();
-  componenteFilterCtrl: FormControl = new FormControl();
+  causaFilterCtrl: FormControl = new FormControl();
   acaoFilterCtrl: FormControl = new FormControl();
   defeitoFilterCtrl: FormControl = new FormControl();
   protected _onDestroy = new Subject<void>();
@@ -57,7 +55,6 @@ export class RelatorioAtendimentoDetalheFormComponent implements OnInit, OnDestr
   ngOnInit(): void {
     this.inicializarForm();
     this.registrarEmitters();
-    this.obterCausas();
   }
 
   private inicializarForm(): void {
@@ -127,36 +124,6 @@ export class RelatorioAtendimentoDetalheFormComponent implements OnInit, OnDestr
         this.obterTiposServico(this.tipoServicoFilterCtrl.value);
       });
 
-    this.moduloFilterCtrl.valueChanges
-      .pipe(
-        takeUntil(this._onDestroy),
-        debounceTime(700),
-        distinctUntilChanged()
-      )
-      .subscribe(() => {
-        this.obterModulos(this.moduloFilterCtrl.value);
-      });
-
-    this.subModuloFilterCtrl.valueChanges
-      .pipe(
-        takeUntil(this._onDestroy),
-        debounceTime(700),
-        distinctUntilChanged()
-      )
-      .subscribe(() => {
-        this.obterSubModulos(this.subModuloFilterCtrl.value);
-      });
-
-    this.componenteFilterCtrl.valueChanges
-      .pipe(
-        takeUntil(this._onDestroy),
-        debounceTime(700),
-        distinctUntilChanged()
-      )
-      .subscribe(() => {
-        this.obterComponentes(this.componenteFilterCtrl.value);
-      });
-
     this.defeitoFilterCtrl.valueChanges
       .pipe(
         takeUntil(this._onDestroy),
@@ -176,13 +143,23 @@ export class RelatorioAtendimentoDetalheFormComponent implements OnInit, OnDestr
       .subscribe(() => {
         this.obterAcoes(this.acaoFilterCtrl.value);
       });
+
+    this.causaFilterCtrl.valueChanges
+      .pipe(
+        takeUntil(this._onDestroy),
+        debounceTime(700),
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        this.obterCausas(this.causaFilterCtrl.value);
+      });
   }
 
   obterTiposServico(filter: string = ''): void {
     this._tipoServicoService.obterPorParametros({
       sortActive: 'nomeServico',
       sortDirection: 'asc',
-      pageSize: 500,
+      pageSize: 50,
       filter: filter
     }).subscribe((data: TipoServicoData) => {
       this.form.controls['codServico'].enable();
@@ -197,60 +174,11 @@ export class RelatorioAtendimentoDetalheFormComponent implements OnInit, OnDestr
     });
   }
 
-  obterModulos(filter: string = ''): void {
-    this.modulos = this.causas.filter(c => c.codECausa.substring(2, 5) === '000');
-
-    if (filter) {
-      let modulos = this.modulos
-        .filter(c => c.codECausa.toLowerCase().includes(filter.toLowerCase()) ||
-          c.nomeCausa.toLowerCase().includes(filter.toLowerCase()));
-
-      if (modulos.length) {
-        this.modulos = modulos;
-      }
-    }
-  }
-
-  obterSubModulos(filter: string = ''): void {
-    let codModulo = this.form.controls['codModulo'].value;
-    let modulo = this.causas.filter(m => m.codCausa === codModulo).shift();
-
-    this.subModulos = this.causas
-      .filter(c => c.codECausa.substring(0, 2) === modulo.codECausa.substring(0, 2) &&
-        c.codECausa.substring(3, 5) === '00');
-
-    if (filter) {
-      let subModulos = this.subModulos
-        .filter(c => c.codECausa.toLowerCase().includes(filter.toLowerCase()) ||
-          c.nomeCausa.toLowerCase().includes(filter.toLowerCase()));
-
-      if (subModulos.length) {
-        this.subModulos = subModulos;
-      }
-    }
-  }
-
-  obterComponentes(filter: string = ''): void {
-    let codSubModulo = this.form.controls['codSubModulo'].value;
-    let subModulo = this.causas.filter(s => s.codCausa === codSubModulo).shift();
-
-    this.componentes = this.causas
-      .filter(c => c.codECausa.substring(0, 3) === subModulo.codECausa.substring(0, 3));
-
-    if (filter) {
-      let componentes = this.componentes
-        .filter(c => c.codECausa.toLowerCase().includes(filter.toLowerCase()) ||
-          c.nomeCausa.toLowerCase().includes(filter.toLowerCase()));
-
-      if (componentes.length) this.componentes = componentes;
-    }
-  }
-
   obterCausas(filter: string = ''): void {
     this._causaService.obterPorParametros({
       sortActive: 'codECausa',
       sortDirection: 'asc',
-      pageSize: 1500,
+      pageSize: 50,
       filter: filter,
       indAtivo: 1
     }).subscribe((data: CausaData) => {
@@ -262,7 +190,7 @@ export class RelatorioAtendimentoDetalheFormComponent implements OnInit, OnDestr
     this._defeitoService.obterPorParametros({
       sortActive: 'codEDefeito',
       sortDirection: 'asc',
-      pageSize: 500,
+      pageSize: 50,
       filter: filter,
       indAtivo: 1
     }).subscribe((data: DefeitoData) => {
@@ -274,7 +202,7 @@ export class RelatorioAtendimentoDetalheFormComponent implements OnInit, OnDestr
     this._acaoService.obterPorParametros({
       sortActive: 'codEAcao',
       sortDirection: 'asc',
-      pageSize: 500,
+      pageSize: 50,
       filter: filter,
       indAtivo: 1
     }).subscribe((data: AcaoData) => {

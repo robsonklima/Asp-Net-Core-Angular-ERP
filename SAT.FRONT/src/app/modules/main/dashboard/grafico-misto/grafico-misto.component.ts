66c3504db22f255789fbb6863734d3bfd562e14f
robson <robsonklima@gmail.com>
2021-08-29 19:10:37 -0300

    REF

--- SAT.FRONT/src/app/modules/main/dashboard/grafico-misto/grafico-misto.component.ts
+++ SAT.FRONT/src/app/modules/main/dashboard/grafico-misto/grafico-misto.component.ts
@@ -36,12 +36,12 @@ export class GraficoMistoComponent {
     this.chartOptions = {
       series: [
         {
-          name: "Website Blog",
+          name: "Indicador",
           type: "column",
           data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
         },
         {
-          name: "Social Media",
+          name: "Meta",
           type: "line",
           data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
         }
@@ -54,39 +54,39 @@ export class GraficoMistoComponent {
         width: [0, 4]
       },
       title: {
-        text: "Traffic Sources"
+        text: "Título do Gráfico"
       },
       dataLabels: {
         enabled: true,
         enabledOnSeries: [1]
       },
       labels: [
-        "01 Jan 2001",
-        "02 Jan 2001",
-        "03 Jan 2001",
-        "04 Jan 2001",
-        "05 Jan 2001",
-        "06 Jan 2001",
-        "07 Jan 2001",
-        "08 Jan 2001",
-        "09 Jan 2001",
-        "10 Jan 2001",
-        "11 Jan 2001",
-        "12 Jan 2001"
+        "JAN",
+        "FEV",
+        "MAR",
+        "ABR",
+        "MAI",
+        "JUN",
+        "JUL",
+        "AGO",
+        "SET",
+        "OUT",
+        "NOV",
+        "DEZ"
       ],
       xaxis: {
-        type: "datetime"
+        type: "category"
       },
       yaxis: [
         {
           title: {
-            text: "Website Blog"
+            text: "Info A"
           }
         },
         {
           opposite: true,
           title: {
-            text: "Social Media"
+            text: "Info B"
           }
         }
       ]

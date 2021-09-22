fdb01de5ce7bcef065de2592785bc99200739641
mariji <mpdsantos@inf.ufpel.edu.com.br>
2021-09-22 17:31:55 -0300

    enumeracao filiais

--- SAT.FRONT/src/app/modules/main/dashboard/tela-mapa/tela-mapa.component.ts
+++ SAT.FRONT/src/app/modules/main/dashboard/tela-mapa/tela-mapa.component.ts
@@ -3,7 +3,8 @@ import { AfterViewInit, Component  } from '@angular/core';
 @Component({
   selector: 'app-tela-mapa',
   templateUrl: './tela-mapa.component.html',
-  styleUrls: ['./tela-mapa.component.css']
+  styleUrls: ['./tela-mapa.component.css'
+  ]
 })
 
 export class TelaMapaComponent implements AfterViewInit 

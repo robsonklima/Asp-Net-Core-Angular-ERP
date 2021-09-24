c681b0072d911710792ce131f27ad4cd4f8fac1d
mariji <mpdsantos@inf.ufpel.edu.com.br>
2021-09-24 16:03:28 -0300

    disponibilidade por regiao bbts

--- SAT.FRONT/src/app/modules/main/dashboard/mapa-disponibilidade/mapa-disponibilidade.component.ts
+++ SAT.FRONT/src/app/modules/main/dashboard/mapa-disponibilidade/mapa-disponibilidade.component.ts
@@ -1,8 +1,20 @@
 import { AfterViewInit, Component  } from '@angular/core';
 import { IndicadorService } from 'app/core/services/indicador.service';
-import { Indicador, IndicadorAgrupadorEnum, IndicadorParameters, IndicadorTipoEnum } from 'app/core/types/indicador.types';
-import { FilialEnum } from 'app/shared/enums/FilialEnum';
-import moment from 'moment';
+
+export enum Region
+{
+  NORTE,
+  SUL,
+  SUDESTE,
+  CENTROESTE,
+  NORDESTE
+}
+
+export type StateByRegion =
+{
+  region: Region;
+  state: SVGPathElement;
+};
 
 @Component({
   selector: 'app-mapa-disponibilidade',
@@ -13,110 +25,93 @@ import moment from 'moment';
 
 export class MapaDisponibilidadeComponent implements AfterViewInit 
 {
-  private paths: SVGPathElement[] = [];
-  private ellipses: SVGRectElement[] = [];
-  private codFiliais: string[] = [];
-  
+  private statesByRegion: StateByRegion[] = [];
+
   constructor(private _indicadorService: IndicadorService) { }
+
   ngAfterViewInit(): void
   {
-    this.getPaths();
-    this.getFiliais();
+    this.getStates();
     this.initializeMap();
   }
 
-  public addElements(p: SVGPathElement): void
+  public initializeMap(): void
+  {
+    this.getStates();
+    this.addRects();
+  }
+
+  public async getData(): Promise<void>
+  {
+    
+  }
+
+  private getStates(): void
+  {
+    var states = Array.from(document.querySelector("#landmarks-brazil-disp").querySelectorAll("path"));
+
+    states.forEach(s =>
+    {
+      const sbr: StateByRegion =
+      {
+        region: this.getRegion(this.getInitials(s.id)),
+        state: s
+      };
+
+      this.statesByRegion.push(sbr);
+      this.addElements(sbr.region, s);
+    })
+  }
+
+  private addRects(): void
+  {
+    // NORTE
+    // var norte = this.statesByRegion.find(i => i.state.id.startsWith("rn"));
+    // var t = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
+    // t.cx.baseVal.value = 80;
+    // t.cy.baseVal.value = 80;
+    // t.rx.baseVal.value = 80;
+    // t.ry.baseVal.value = 80;
+    // var b = norte.state.getBBox();
+    // t.setAttribute("transform", "translate(" + (b.x + b.width/0.5) + " " + (b.y + b.height/4) + ")");
+    // t.setAttribute("fill", this.getRegionColor(norte.region));
+    // norte.state.parentNode.insertBefore(t, norte.state.nextSibling);
+  }
+
+  private getRegion(id: string): Region
+  {
+    if ("AM, RR, AC, PA, TO, AP, RO".includes(id)) return Region.NORTE;
+    if ("RS, SC, PR".includes(id)) return Region.SUL;
+    if ("SP, RJ, MG, ES".includes(id)) return Region.SUDESTE;
+    if ("MT, MS, GO, DF".includes(id)) return Region.CENTROESTE;
+    if ("MA, PI, PE, PB, RN, BA, SE, AL, CE".includes(id)) return Region.NORDESTE;
+  }
+
+  private getRegionColor(r: Region): string
+  {
+    return r == Region.SUL ? "#e77094" :
+              r == Region.NORDESTE ? "#e89d7b" : 
+                r == Region.NORTE ? "#7823a2" : 
+                  r == Region.CENTROESTE ? "#08ddc8" : "#c8dc72";
+  }
+
+  public addElements(r: Region, p: SVGPathElement): void
   {
       var t = document.createElementNS("http://www.w3.org/2000/svg", "text");
       var b = p.getBBox();
       t.setAttribute("transform", "translate(" + (b.x + b.width/1.6) + " " + (b.y + b.height/1.7) + ")");
-      t.textContent = p.id.toUpperCase().split("-").shift();
+      t.textContent = this.getInitials(p.id);
       t.setAttribute("fill", "black");
       t.setAttribute("pointer-events", "none");
       t.setAttribute("text-anchor", "middle");
       t.setAttribute("font-weight", "bold");
       t.setAttribute("font-size", "12");
+      p.setAttribute("fill", this.getRegionColor(r));
       p.parentNode.insertBefore(t, p.nextSibling);
-
-      if (p.id.startsWith("sul")){
-        var c = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        c.rx.baseVal.value = 80;
-        c.ry.baseVal.value = 80;
-        c.style.fill = "blue";
-        c.setAttribute("cursor", "pointer");
-        c.setAttribute("transform", "translate(" + (b.x + b.width/2) + " " + (b.y + b.height/2) + ")");
-        c.setAttribute("id", p.id);
-        p.parentNode.insertBefore(c, t);
-      }
   }
 
-  public initializeMap(): void
+  private getInitials(id: string): string
   {
-    for (var p in this.paths)
-      this.addElements(this.paths[p]);
-
-    //this.getEllipses();
-    this.getData();
-  }
-
-  public async getData(): Promise<void>
-  {
-    var params: IndicadorParameters = 
-    {
-      agrupador: IndicadorAgrupadorEnum.FILIAL,
-      tipo: IndicadorTipoEnum.SLA,
-      codFiliais: this.codFiliais.join(','),
-      dataInicio: moment().startOf('month').toISOString(),
-      dataFim: moment().endOf('month').toISOString()
-    };
-
-    var data = await this._indicadorService.obterPorParametros(params).toPromise();
-    this.updateData(data);
-  }
-
-  private getPaths(): void
-  {
-    this.paths = Array.from(document.querySelector("#landmarks-brazil-disp").querySelectorAll("path"));
-  }
-
-  private getFiliais(): void
-  {
-    Object.keys(FilialEnum).filter((e) => isNaN(Number(e))).forEach((i) => this.codFiliais.push(this.parseFilialCod(i)));
-  }
-
-  private getEllipses(): void
-  {
-    this.ellipses = Array.from(document.querySelector("#landmarks-brazil-disp").querySelectorAll("rect"));
-  }
-
-  private updateData(data: Indicador[]): void
-  {
-    data.forEach(d => this.paintEllipse(d));
-  }
-
-  private paintEllipse(filial: Indicador)
-  {
-    var c = this.ellipses.find(c => c.id.toLocaleUpperCase() == filial.label.toUpperCase());
-    c.style.fill = filial.valor >= 95 ? 'green' : filial.valor >= 90 ? 'yellow' : 'red';
-  }
-
-  private parseFilialCod(sigla: string): string
-  {
-    return FilialEnum[sigla].toString();
-  }
-
-  private selectedEllipse(this, ev: MouseEvent)
-  {
-    alert("oi, eu sou a filial " + (this as SVGEllipseElement).id + ", cod " + FilialEnum[(this as SVGEllipseElement).id.toUpperCase()]);
-  }
-
-  private highlightEllipse(this, ev: MouseEvent)
-  {
-    // todo
-  }
-
-  private unhighlightEllipse(this, ev: MouseEvent)
-  {
-    // todo
+    return id.toUpperCase().split("-").shift();
   }
 }

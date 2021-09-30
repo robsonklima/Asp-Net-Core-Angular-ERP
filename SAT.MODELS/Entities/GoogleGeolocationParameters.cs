7fa50950b980c788715a66a53ba46bd78ad59353
Majerkowski <endriumajerkowski@gmail.com>
2021-09-30 17:06:19 -0300

    Status das filiais

---
+++ SAT.MODELS/Entities/GoogleGeolocationParameters.cs
@@ -0,0 +1,9 @@
+﻿using SAT.MODELS.Entities.Helpers;
+
+namespace SAT.MODELS.Entities
+{
+    public class GoogleGeolocationParameters : QueryStringParameters
+    {
+        public string EnderecoCep { get; set; }
+    }
+}

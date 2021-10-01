323c9afe3406e369fc408466a9946b1c8e2dabf2
robsonklima <robsonklima@gmail.com>
2021-10-01 14:26:41 -0300

    instalacao do Mobiscroll

--- SAT.FRONT/src/app/modules/main/agenda-tecnico/agenda-tecnico.component.ts
+++ SAT.FRONT/src/app/modules/main/agenda-tecnico/agenda-tecnico.component.ts
@@ -1,22 +1,128 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { setOptions, MbscEventcalendarView, MbscCalendarEvent, Notifications } from '@mobiscroll/angular';
+import { HttpClient } from '@angular/common/http';
+
+setOptions({
+    //locale: localePtBR,
+    theme: 'ios',
+    themeVariant: 'light',
+    clickToCreate: true,
+    dragToCreate: true,
+    dragToMove: true,
+    dragToResize: true
+});
 
 @Component({
-    selector       : 'app-agenda-tecnico',
-    templateUrl    : './agenda-tecnico.component.html',
-    styleUrls      : ['./agenda-tecnico.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    encapsulation  : ViewEncapsulation.None
+    selector: 'app-agenda-tecnico',
+    templateUrl: './agenda-tecnico.component.html',
+    providers: [Notifications]
 })
 export class AgendaTecnicoComponent implements OnInit {
-    
-    constructor(
-        
-    ) {
-        
+
+    constructor(private http: HttpClient, private notify: Notifications) {}
+
+    myEvents: MbscCalendarEvent[] = [];
+    view = 'week';
+    calView: MbscEventcalendarView = {
+        timeline: {
+            type: 'week',
+            timeCellStep: 720,
+            timeLabelStep: 1440
+        }
+    };
+
+    myResources = [{
+        id: 1,
+        name: 'Ryan',
+        color: '#ff0101',
+        title: 'Cloud System Engineer',
+        img: 'https://img.mobiscroll.com/demos/m1.png'
+    }, {
+        id: 2,
+        name: 'Kate',
+        color: '#239a21',
+        title: 'Help Desk Specialist',
+        img: 'https://img.mobiscroll.com/demos/f1.png'
+    }, {
+        id: 3,
+        name: 'John',
+        color: '#8f1ed6',
+        title: 'Application Developer',
+        img: 'https://img.mobiscroll.com/demos/m2.png'
+    }, {
+        id: 4,
+        name: 'Mark',
+        color: '#01adff',
+        title: 'Network Administrator',
+        img: 'https://img.mobiscroll.com/demos/m3.png'
+    }, {
+        id: 5,
+        name: 'Sharon',
+        color: '#d8ca1a',
+        title: 'Data Quality Manager',
+        img: 'https://img.mobiscroll.com/demos/f2.png'
+    }];
+
+    myInvalids = [{
+        start: '00:00',
+        end: '06:00',
+        recurring: {
+            repeat: 'weekly',
+            weekDays: 'MO,TU,WE,TH,FR'
+        }
+    }, {
+        start: '20:00',
+        end: '23:59',
+        recurring: {
+            repeat: 'weekly',
+            weekDays: 'MO,TU,WE,TH,FR'
+        }
+    }, {
+        recurring: {
+            repeat: 'weekly',
+            weekDays: 'SA,SU'
+        }
+    }];
+
+    ngOnInit(): void {
+        this.http.jsonp < MbscCalendarEvent[] > ('https://trial.mobiscroll.com/timeline-events/', 'callback').subscribe((resp) => {
+            this.myEvents = resp;
+        });
     }
-    
-    ngOnInit(): void
-    {
-        
+
+    changeView(): void {
+        setTimeout(() => {
+            switch (this.view) {
+                case 'day':
+                    this.calView = {
+                        timeline: { type: 'day' }
+                    };
+                    break;
+                case 'workweek':
+                    this.calView = {
+                        timeline: {
+                            type: 'week',
+                            startDay: 1,
+                            endDay: 5
+                        }
+                    };
+                    break;
+                case 'week':
+                    this.calView = {
+                        timeline: {
+                            type: 'week',
+                            timeCellStep: 720,
+                            timeLabelStep: 1440
+                        }
+                    };
+                    break;
+            }
+        });
+    }
+
+    eventUpdateFail(): void {
+        this.notify.toast({
+            message: 'Can\'t schedule outside of working hours'
+        });
     }
 }
--- SAT.FRONT/src/app/modules/main/agenda-tecnico/agenda-tecnico.module.ts
+++ SAT.FRONT/src/app/modules/main/agenda-tecnico/agenda-tecnico.module.ts
@@ -1,28 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MAT_DATE_FORMATS } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { FullCalendarModule } from '@fullcalendar/angular';
-import { FuseDateRangeModule } from '@fuse/components/date-range';
-import { SharedModule } from 'app/shared/shared.module';
 import { AgendaTecnicoComponent } from 'app/modules/main/agenda-tecnico/agenda-tecnico.component';
 import { agendaTecnicoRoutes } from 'app/modules/main/agenda-tecnico/agenda-tecnico.routing';
 import { TranslocoModule } from '@ngneat/transloco';
-import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MbscModule } from '@mobiscroll/angular';
+import { SharedModule } from 'app/shared/shared.module';
 
 export const FORMATO_DATA = {
     parse: {
@@ -40,30 +26,17 @@ export const FORMATO_DATA = {
     declarations: [
         AgendaTecnicoComponent,
     ],
-    imports     : [
+    imports: [
         RouterModule.forChild(agendaTecnicoRoutes),
-        ScrollingModule,
-        MatButtonModule,
-        MatButtonToggleModule,
-        MatCheckboxModule,
-        MatDatepickerModule,
-        MatDialogModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatMenuModule,
-        MatMomentDateModule,
-        MatRadioModule,
-        MatSelectModule,
-        MatSidenavModule,
-        MatTooltipModule,
-        FullCalendarModule,
-        FuseDateRangeModule,
-        NgxMatSelectSearchModule,
+        TranslocoModule,
         SharedModule,
-        TranslocoModule
+        MbscModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        HttpClientJsonpModule
     ],
-    providers   : [
+    providers: [
         {
             provide : MAT_DATE_FORMATS,
             useValue: FORMATO_DATA

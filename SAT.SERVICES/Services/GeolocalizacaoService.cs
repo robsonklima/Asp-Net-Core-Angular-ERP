b8a62eb69f6fb5c9d33ce7f29d65e485d6870160
Majerkowski <endriumajerkowski@gmail.com>
2021-10-05 15:29:34 -0300

    Dashboard - Disponibilidade Técnicos

---
+++ SAT.SERVICES/Services/GeolocalizacaoService.cs
@@ -0,0 +1,30 @@
+﻿using SAT.INFRA.Interfaces;
+using SAT.MODELS.Entities;
+using SAT.MODELS.Helpers;
+using SAT.MODELS.ViewModels;
+using SAT.SERVICES.Interfaces;
+
+namespace SAT.SERVICES.Services
+{
+    public class GeolocalizacaoService : IGeolocalizacaoService
+    {
+        private readonly IGeolocalizacaoRepository _iGeolocalizacaoRepository;
+
+        public GeolocalizacaoService(IGeolocalizacaoRepository iGeolocalizacaoRepository)
+        {
+            this._iGeolocalizacaoRepository = iGeolocalizacaoRepository;
+        }
+
+        public Geolocalizacao Criar(Geolocalizacao geolocalizacao)
+        {
+            this._iGeolocalizacaoRepository.Criar(geolocalizacao);
+            return geolocalizacao;
+        }
+
+        public ListViewModel ObterPorParametros(GoogleGeolocationParameters parameters)
+        {
+            PagedList<Geolocalizacao> localizacoes = _iGeolocalizacaoRepository.ObterPorParametros(parameters);
+            return new ListViewModel() { Items = localizacoes };
+        }
+    }
+}

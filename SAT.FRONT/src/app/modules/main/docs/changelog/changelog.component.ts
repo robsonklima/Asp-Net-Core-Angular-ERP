d8ec9f427ae6170c9ae3fda57b821473d5159304
quantumbogosort <mpdsantos@inf.ufpel.edu.br>
2021-10-19 10:08:44 -0300

    changelog 1.0.1

--- SAT.FRONT/src/app/modules/main/docs/changelog/changelog.component.ts
+++ SAT.FRONT/src/app/modules/main/docs/changelog/changelog.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-    selector       : 'changelog',
-    templateUrl    : './changelog.component.html',
-    styles         : [''],
+    selector: 'changelog',
+    templateUrl: './changelog.component.html',
+    styles: [''],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChangelogComponent
 {
     changelog: any[] = [
         {
-            version    : 'v1.0.0',
+            version: 'v1.0.0',
             releaseDate: '01 de Março de 2021',
-            changes    : [
+            changes: [
                 {
                     type: 'Adições',
                     list: [
@@ -20,8 +20,32 @@ export class ChangelogComponent
                     ]
                 }
             ]
+        },
+        {
+            version: 'v1.0.1',
+            releaseDate: '19 de Outubro de 2021',
+            changes: [
+                {
+                    type: 'Adições',
+                    list: [
+                        'Opção de cancelamento de transferência de chamado',
+                        'Filtro pelo técnico na tela de chamados',
+                        'Informações sobre o técnico ao passar o mouse sobre a coluna',
+                        'Status de peças faltantes ao passar o mouse sobre a coluna',
+                        'Coluna de status de SLA',
+                    ],
+
+                },
+                {
+                    type: 'Correções',
+                    list: [
+                        'Mantém filtro na tela de chamados',
+                        'Nome do técnico apenas para chamados transferidos',
+                    ],
+                }
+            ]
         }
     ];
 
-    constructor() {}
+    constructor () { }
 }

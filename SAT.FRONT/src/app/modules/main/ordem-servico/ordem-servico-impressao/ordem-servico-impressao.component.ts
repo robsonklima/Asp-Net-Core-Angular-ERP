761505e65a9c6bf4a0769c923ec7a2fe5a5be050
PERTO\marilia.santos <mpdsantos@inf.ufpel.edu.br>
2021-10-22 16:44:13 -0300

    impressao os

--- SAT.FRONT/src/app/modules/main/ordem-servico/ordem-servico-impressao/ordem-servico-impressao.component.ts
+++ SAT.FRONT/src/app/modules/main/ordem-servico/ordem-servico-impressao/ordem-servico-impressao.component.ts
@@ -22,4 +22,13 @@ export class OrdemServicoImpressaoComponent implements OnInit
     this.codOS = +this._route.snapshot.paramMap.get('codOS');
     this.os = await this._ordemServicoService.obterPorCodigo(this.codOS).toPromise();
   }
+
+  print()
+  {
+    var contentToPrint = document.getElementById("print-area").innerHTML;
+    var windowPopup = window.open('', '_blank', 'width=500,height=500');
+    windowPopup.document.open();
+    windowPopup.document.write('<html><head><link rel="stylesheet" type="text/css" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" /></head><body onload = "window.print()" > ' + contentToPrint + ' </body></html > ');
+    windowPopup.document.close();
+  }
 }

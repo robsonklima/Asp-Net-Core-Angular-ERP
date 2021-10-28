5d11a078a05b69d275bbfe184cd137c08977d3bd
PERTO\marilia.santos <mpdsantos@inf.ufpel.edu.br>
2021-10-28 14:37:26 -0300

    ajuste view despesa tecnico

--- SAT.FRONT/src/app/modules/main/despesa/despesa-atendimento-lista/despesa-atendimento-lista.component.ts
+++ SAT.FRONT/src/app/modules/main/despesa/despesa-atendimento-lista/despesa-atendimento-lista.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fuseAnimations } from '@fuse/animations';
 import { DespesaPeriodoTecnicoService } from 'app/core/services/despesa-periodo-tecnico.service';
 import { DespesaPeriodoService } from 'app/core/services/despesa-periodo.service';
-import { DespesaPeriodo, DespesaPeriodoTecnico } from 'app/core/types/despesa-periodo.types';
+import { DespesaPeriodo, DespesaPeriodoTecnico, DespesaPeriodoTecnicoData } from 'app/core/types/despesa-periodo.types';
 import { UserService } from 'app/core/user/user.service';
 import { UserSession } from 'app/core/user/user.types';
 
 @Component({
   selector: 'app-despesa-atendimento-lista',
-  templateUrl: './despesa-atendimento-lista.component.html'
+  templateUrl: './despesa-atendimento-lista.component.html',
+  styles: [`
+        .list-grid-despesa-atendimento {
+            grid-template-columns: 130px 130px 130px 130px 130px 130px 130px 130px  auto;
+            @screen sm { grid-template-columns: 130px 130px 130px 130px 130px 130px 130px 130px  auto; }
+            @screen md { grid-template-columns: 130px 130px 130px 130px 130px 130px 130px 130px  auto; }
+            @screen lg { grid-template-columns: 130px 130px 130px 130px 130px 130px 130px 130px  auto; }
+        }
+    `],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations
 })
 
 export class DespesaAtendimentoListaComponent implements OnInit
 {
   userSession: UserSession;
   isLoading: boolean = false;
+  dataSourceData: DespesaPeriodoTecnicoData;
   despesasPeriodoTecnico: DespesaPeriodoTecnico[] = [];
   despesasPeriodo: DespesaPeriodo[] = [];
 
   constructor (
-    private _formBuilder: FormBuilder,
     private _userService: UserService,
     private _despesaPeriodoSvc: DespesaPeriodoService,
     private _despesaPeriodoTecnicoSvc: DespesaPeriodoTecnicoService)
@@ -47,5 +57,10 @@ export class DespesaAtendimentoListaComponent implements OnInit
       indAtivo: 1,
       pageSize: 500,
     }).toPromise()).items;
+  }
+
+
+  paginar()
+  {
   }
 }

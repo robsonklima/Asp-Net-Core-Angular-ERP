8b05d39551d0280c6682c398ad3f52c5b892366a
Majerkowski <endriumajerkowski@gmail.com>
2021-11-09 13:38:33 -0300

    OS

--- SAT.FRONT/src/app/core/types/ordem-servico.types.ts
+++ SAT.FRONT/src/app/core/types/ordem-servico.types.ts
@@ -1,192 +1,244 @@
-import { AgendaTecnico } from "./agenda-tecnico.types";
-import { Agendamento } from "./agendamento.types";
-import { Autorizada } from "./autorizada.types";
-import { Cliente } from "./cliente.types";
-import { EquipamentoContrato } from "./equipamento-contrato.types";
-import { Equipamento } from "./equipamento.types";
-import { Filial } from "./filial.types";
-import { Foto } from "./foto.types";
-import { Meta, QueryStringParameters } from "./generic.types";
-import { LocalAtendimento } from "./local-atendimento.types";
-import { OSPrazoAtendimento } from "./os-prazo-atendimento.types";
-import { RegiaoAutorizada } from "./regiao-autorizada.types";
-import { Regiao } from "./regiao.types";
-import { RelatorioAtendimento } from "./relatorio-atendimento.types";
-import { StatusServico } from "./status-servico.types";
-import { Tecnico } from "./tecnico.types";
-import { TipoIntervencao } from "./tipo-intervencao.types";
-
-export class OrdemServico
+import { FileService } from './../../../../core/services/file.service';
+import
 {
-    codOS: number;
-    statusServico?: StatusServico;
-    tipoIntervencao?: TipoIntervencao;
-    localAtendimento?: LocalAtendimento;
-    equipamentoContrato?: EquipamentoContrato;
-    equipamento?: Equipamento;
-    filial?: Filial;
-    regiao?: Regiao;
-    autorizada?: Autorizada;
-    regiaoAutorizada?: RegiaoAutorizada;
-    relatoriosAtendimento: RelatorioAtendimento[];
-    fotos: Foto[];
-    cliente: Cliente;
-    tecnico: Tecnico;
-    agendamentos: Agendamento[];
-    defeitoRelatado: string;
-    observacaoCliente?: string;
-    dataHoraCad: string;
-    dataHoraTransf: string;
-    dataHoraOSMobileLida: string;
-    dataHoraFechamento: string;
-    codUsuarioCad?: any;
-    nomeSolicitante?: any;
-    telefoneSolicitante?: any;
-    codCliente: number;
-    codPosto: number;
-    codEquipContrato: number;
-    codEquip: number;
-    codTipoIntervencao: number;
-    codFilial: number;
-    codAutorizada: number;
-    codRegiao: number;
-    codStatusServico: number;
-    codTecnico: number;
-    descNumSerieNi?: any;
-    dataHoraSolicitacao: string;
-    numOSCliente?: any;
-    numOSQuarteirizada?: string;
-    nomeContato?: any;
-    descMotivoMarcaEspecial?: any;
-    indMarcaEspecial: number;
-    codUsuarioMarcaEspecial?: any;
-    dataMarcaEspecial?: any;
-    codUsuarioFechamento: string;
-    motivoCancelamento?: any;
-    codUsuarioCancelamento?: any;
-    dataHoraCancelamento?: any;
-    indRevisaoReincidencia: number;
-    indCienciaAtendente: number;
-    indCienciaTerceirizada: number;
-    indServico?: any;
-    servicoEmail?: any;
-    numAgenciaNI?: any;
-    numRemessa?: any;
-    codUsuarioManut: string;
-    dataHoraManut: string;
-    codTipoMarcacao?: any;
-    tempoSlaInicio: string;
-    tempoSlaReparo: string;
-    tempoSlaSolucao: string;
-    endereco?: any;
-    bairro?: any;
-    cidade?: any;
-    siglaUF?: any;
-    pais?: any;
-    cep?: any;
-    numPatrimonio?: any;
-    dataHoraAberturaOS: string;
-    dataHoraSolicAtendimento?: any;
-    indCancelado: number;
-    indReincidencia?: any;
-    dataCadastro: string;
-    codUsuarioCadastro: string;
-    dataManutencao: string;
-    codUsuarioManutencao: string;
-    indAtrasoSLA: number;
-    tempoEfetInicio?: any;
-    tempoEfetReparo?: any;
-    tempoEfetSolucao?: any;
-    indAcertoParque: number;
-    indEndossado: number;
-    codUsuarioAcertoParque?: any;
-    dataAcertoParque?: any;
-    indRevOk?: any;
-    codTipoEquip: number;
-    codGrupoEquip: number;
-    indOrcamentoEnd?: any;
-    indEnderecoRevisado: number;
-    dataHoraEnderecoVerificado?: any;
-    dataHoraIntegracaoRevisao?: any;
-    dataHoraIntegracaoRevisaoAgendamento?: any;
-    indAgendamentoReenviado?: any;
-    dataHoraOsmobileRecebida?: any;
-    dataHoraOsmobileLida: string;
-    numAgenciaBanco?: any;
-    numContaEstabelecimentoCliente?: any;
-    cnpjestabelecimentoCliente?: any;
-    razaoSocialEstabelecimentoCliente?: any;
-    redeEquipamento?: any;
-    numTerminal?: any;
-    indServicoVerificado?: any;
-    indIntegracao: number;
-    dataHoraEnvioAgendamentoSemat?: any;
-    indAgendamentoUnico?: any;
-    indNotificacaoOrcamentoEnviado?: any;
-    codMotivoCancelamentoBanrisul?: any;
-    nomeArquivoIntegracaoBanrisul?: any;
-    indReaberturaIntegracaoBanrisul?: any;
-    indOSIntervencaoEquipamento?: any;
-    indLiberacaoFechaduraCofre: number;
-    indExclusaoBanrisul?: any;
-    dataExclusaoBanrisul?: any;
-    indBloqueioReincidencia: number;
-    numReincidencia: number;
-    indVandalismo?: any;
-    indStatusEnvioReincidencia: number;
-    indFechamentoBanrisul?: any;
-    dataFechamentoBanrisul?: any;
-    obsFechamentoBanrisul?: any;
-    numEstabelecimentoCliente?: any;
-    codSeveridade?: number;
-    codContrato?: number;
-    sugestaoOS?: any;
-    codMotivoCancelamento?: number;
-    codOperadoraTelefonia?: number;
-    codDefeitoPOS?: number;
-    codUsuarioOSMobileLida: string;
-    codUsuarioOsmobileRecebida?: string;
-    dataHoraIntegracaoRevisaoV2?: string;
-    dataHoraIntegracaoRevisaoAgendamentoV2?: any;
-    prazosAtendimento: OSPrazoAtendimento[];
-    indNumRATObrigatorio?: boolean;
-    agendaTecnico?: AgendaTecnico;
-}
+    AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild, ViewEncapsulation
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { fromEvent, interval, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith, takeUntil } from 'rxjs/operators';
+import { fuseAnimations } from '@fuse/animations';
+import { UserService } from 'app/core/user/user.service';
+import { UserSession } from 'app/core/user/user.types';
+import { MatSort } from '@angular/material/sort';
+import { OrdemServico, OrdemServicoData, OrdemServicoParameters } from 'app/core/types/ordem-servico.types';
+import { OrdemServicoService } from 'app/core/services/ordem-servico.service';
+import { MatSidenav } from '@angular/material/sidenav';
+import { FileMime } from 'app/core/types/file.types';
+import Enumerable from 'linq';
+import moment from 'moment';
 
-export interface OrdemServicoData extends Meta
+@Component({
+    selector: 'ordem-servico-lista',
+    templateUrl: './ordem-servico-lista.component.html',
+    styles: [`
+        .list-grid-ordem-servico {
+            grid-template-columns: 30px 60px 72px 60px 20px 48px 50px 30px auto 140px 40px 100px 50px 36px 140px 29px;
+            
+            @screen sm {
+                grid-template-columns:  48px 72px 92px 92px 36px 36px auto 56px;
+            }
+        
+            @screen md {
+                grid-template-columns: 48px 92px 92px 92px 38px 36px auto 58px 58px 58px 58px 58px;
+            }
+        
+            @screen lg {
+                grid-template-columns: 30px 60px 72px 60px  20px 48px 50px 30px auto 140px 40px 100px 50px 36px 140px 29px;
+            }
+        }
+    `],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
+})
+export class OrdemServicoListaComponent implements AfterViewInit
 {
-    items: OrdemServico[];
-};
+    @ViewChild('sidenav') sidenav: MatSidenav;
+    userSession: UserSession;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) private sort: MatSort;
+    dataSourceData: OrdemServicoData;
+    selectedItem: OrdemServico | null = null;
+    filtro: any;
+    isLoading: boolean = false;
+    @ViewChild('searchInputControl', { static: true }) searchInputControl: ElementRef;
+    protected _onDestroy = new Subject<void>();
 
-export interface OrdemServicoParameters extends QueryStringParameters
-{
-    codOS?: string;
-    codEquipContrato?: number;
-    codTecnico?: number;
-    numOSCliente?: string;
-    numOSQuarteirizada?: string;
-    dataAberturaInicio?: string;
-    dataAberturaFim?: string;
-    dataFechamentoInicio?: string;
-    dataFechamentoFim?: string;
-    codStatusServicos?: string,
-    codTiposIntervencao?: string;
-    codClientes?: string;
-    codFiliais?: string;
-    pa?: number;
-    dataTransfInicio?: string;
-    dataTransfFim?: string;
-    include?: OrdemServicoIncludeEnum;
-    filterType?: OrdemServicoFilterEnum;
-};
+    constructor (
+        private _cdr: ChangeDetectorRef,
+        private _ordemServicoService: OrdemServicoService,
+        private _userService: UserService,
+        private _fileService: FileService
+    )
+    {
+        this.userSession = JSON.parse(this._userService.userSession);
+    }
 
-export enum OrdemServicoIncludeEnum
-{
-    OS_RAT = 1,
-    OS_AGENDA = 2
-}
+    ngAfterViewInit(): void
+    {
+        this.carregarFiltro();
+        interval(5 * 60 * 1000)
+            .pipe(
+                startWith(0),
+                takeUntil(this._onDestroy)
+            )
+            .subscribe(() =>
+            {
+                this.obterOrdensServico();
+            });
 
-export enum OrdemServicoFilterEnum
-{
-    FILTER_AGENDA = 1
+        this.registrarEmitters();
+
+        fromEvent(this.searchInputControl.nativeElement, 'keyup').pipe(
+            map((event: any) =>
+            {
+                return event.target.value;
+            })
+            , debounceTime(1000)
+            , distinctUntilChanged()
+        ).subscribe((text: string) =>
+        {
+            this.paginator.pageIndex = 0;
+            this.obterOrdensServico(text);
+        });
+
+        if (this.sort && this.paginator)
+        {
+            this.sort.disableClear = true;
+            this._cdr.markForCheck();
+
+            this.sort.sortChange.subscribe(() =>
+            {
+                this._userService.atualizarPropriedade("ordem-servico", "sortActive", this.sort.active);
+                this._userService.atualizarPropriedade("ordem-servico", "sortDirection", this.sort.direction);
+                this.carregarFiltro();
+                this.paginator.pageIndex = 0;
+                this.obterOrdensServico();
+            });
+        }
+
+        this._cdr.detectChanges();
+    }
+
+    async obterOrdensServico(filter: string = '')
+    {
+        this.isLoading = true;
+
+        const params: OrdemServicoParameters = {
+            pageNumber: this.paginator.pageIndex + 1,
+            sortActive: this.filtro?.parametros?.sortActive || this.sort.active || 'codOS',
+            sortDirection: this.filtro?.parametros?.direction || this.sort.direction || 'desc',
+            pageSize: this.filtro?.parametros?.qtdPaginacaoLista ?? this.paginator?.pageSize,
+            filter: filter
+        };
+
+        const data: OrdemServicoData = await this._ordemServicoService
+            .obterPorParametros({
+                ...params,
+                ...this.filtro?.parametros
+            })
+            .toPromise();
+
+        this.dataSourceData = data;
+        this.isLoading = false;
+    }
+
+    private registrarEmitters(): void
+    {
+        this.sidenav.closedStart.subscribe(() =>
+        {
+            this.paginator.pageIndex = 0;
+            this.carregarFiltro();
+            this.obterOrdensServico();
+        })
+    }
+
+    private carregarFiltro(): void
+    {
+        this.filtro = this._userService.obterFiltro('ordem-servico');
+        if (!this.filtro)
+        {
+            return;
+        }
+
+        // Filtro obrigatorio de filial quando o usuario esta vinculado a uma filial
+        if (this.userSession?.usuario?.codFilial)
+        {
+            this.filtro.parametros.codFiliais = [this.userSession.usuario.codFilial]
+        }
+
+        Object.keys(this.filtro?.parametros).forEach((key) =>
+        {
+            if (this.filtro?.parametros[key] instanceof Array)
+            {
+                this.filtro.parametros[key] = this.filtro.parametros[key].join()
+            };
+        });
+    }
+
+    public async exportar()
+    {
+        this.isLoading = true;
+        const params: OrdemServicoParameters = {
+            sortDirection: 'desc',
+            pageSize: 100000,
+        };
+
+        window.open(await this._fileService.downloadLink("OrdemServico", FileMime.Excel, {
+            ...this.filtro?.parametros,
+            ...params
+        }));
+        this.isLoading = false;
+    }
+
+    paginar() 
+    {
+        this._userService.atualizarPropriedade(this.filtro?.nome, "qtdPaginacaoLista", this.paginator?.pageSize);
+        this.obterOrdensServico();
+    }
+
+    ngOnDestroy()
+    {
+        this._onDestroy.next();
+        this._onDestroy.complete();
+    }
+
+    statusSLADescricao(os: OrdemServico)
+    {
+        if (os.prazosAtendimento == null)
+        {
+            return "---";
+        }
+        else if (os.statusServico?.codStatusServico == 3 && os.prazosAtendimento?.length > 0)
+        {
+            if (os.dataHoraFechamento < os.prazosAtendimento[os.prazosAtendimento.length - 1]?.dataHoraLimiteAtendimento)
+                return "DENTRO";
+            return "FORA";
+        }
+        else if (os.prazosAtendimento?.length > 0)
+        {
+            var now = moment();
+            var limit = moment(os.prazosAtendimento[os.prazosAtendimento.length - 1]?.dataHoraLimiteAtendimento);
+            if (now < limit)
+                return "DENTRO";
+            return "FORA";
+        }
+        return "---";
+    }
+
+    statusServicoDescricao(os: OrdemServico)
+    {
+        var description = os.statusServico?.nomeStatusServico;
+
+        if (os.statusServico?.codStatusServico == 7 || os.statusServico?.codStatusServico == 10)
+        {
+            var pecas = Enumerable.from(os.relatoriosAtendimento)
+                .selectMany(rat => Enumerable.from(rat.relatorioAtendimentoDetalhes)
+                    .selectMany(d => Enumerable.from(d.relatorioAtendimentoDetalhePecas)
+                        .select(dp => dp.peca?.codMagnus))).toArray();
+
+            if (pecas.length > 0) description = description + "\nPEÇAS: " + pecas.join(", ");
+        }
+
+        return description;
+    }
+
+    tecnicoDescricao(os: OrdemServico)
+    {
+        var description = os.tecnico?.nome;
+        description += '\n' + 'TRANSFERIDO EM: ';
+        description += os.dataHoraTransf ? moment(os.dataHoraTransf).format('DD/MM HH:mm') + '\n' : 'NÃO DISPONÍVEL\n';
+        description += 'VISUALIZADO EM: ';
+        description += os.dataHoraOSMobileLida ? moment(os.dataHoraOSMobileLida).format('DD/MM HH:mm') : 'NÃO VISUALIZADO';
+        return description;
+    }
 }

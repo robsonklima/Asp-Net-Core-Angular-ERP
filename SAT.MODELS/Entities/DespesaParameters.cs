a0bf3f6c57a850bafca8f32326f5a48526147b77
PERTO\marilia.santos <mpdsantos@inf.ufpel.edu.br>
2021-11-26 16:18:05 -0300

    ref

--- SAT.MODELS/Entities/DespesaParameters.cs
+++ SAT.MODELS/Entities/DespesaParameters.cs
@@ -6,7 +6,7 @@ namespace SAT.MODELS.Entities
     public class DespesaParameters : QueryStringParameters
     {
         public int? CodDespesaPeriodo { get; set; }
-        public string? CodTecnico { get; set; }
+        public string CodTecnico { get; set; }
         public string CodRATs { get; set; }
         public DateTime? DataHoraInicioRAT { get; set; }
     }

0822cf4620f1303ae5488d50d7fd1fe2a3cb51ae
Majerkowski <endriumajerkowski@gmail.com>
2021-12-02 14:47:41 -0300

    Dashboard

---
+++ SAT.API/Support/EnumExtension.cs
@@ -0,0 +1,18 @@
+﻿using SAT.MODELS.Enums;
+using System;
+using System.ComponentModel;
+
+namespace SAT.API.Support
+{
+    public static class EnumExtension
+    {
+        public static string ToDescriptionString(this NomeIndicadorEnum val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+}

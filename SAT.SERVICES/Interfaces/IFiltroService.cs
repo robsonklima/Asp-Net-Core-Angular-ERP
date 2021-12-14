777bc1668ef737578da7345c5c6ac7ce0ef79bbb
Majerkowski <endriumajerkowski@gmail.com>
2021-12-14 15:17:59 -0300

    Preset de filtros de usuario

---
+++ SAT.SERVICES/Interfaces/IFiltroService.cs
@@ -0,0 +1,14 @@
+﻿using SAT.MODELS.Entities;
+using SAT.MODELS.ViewModels;
+
+namespace SAT.SERVICES.Interfaces
+{
+    public interface IFiltroService
+    {
+        ListViewModel ObterPorParametros(AcaoParameters parameters);
+        FiltroUsuario Criar(FiltroUsuario filtroUsuario);
+        void Deletar(int codigo);
+        void Atualizar(FiltroUsuario filtroUsuario);
+        FiltroUsuario ObterPorCodigo(int codigo);
+    }
+}

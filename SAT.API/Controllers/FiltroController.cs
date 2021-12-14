08120354074bf2ce1ab5763804bccb2046004bc4
Majerkowski <endriumajerkowski@gmail.com>
2021-12-14 13:32:01 -0300

    Preset de filtros de usuario

---
+++ SAT.API/Controllers/FiltroController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using SAT.MODELS.Entities;
+using SAT.MODELS.ViewModels;
+using SAT.SERVICES.Interfaces;
+
+namespace SAT.API.Controllers
+{
+    [Authorize]
+    [EnableCors("CorsApi")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FiltroController : ControllerBase
+    {
+        private readonly IFiltroService _filtroService;
+
+        public FiltroController(IFiltroService filtroService)
+        {
+            _filtroService = filtroService;
+        }
+
+        [HttpGet]
+        public ListViewModel Get([FromQuery] AcaoParameters parameters)
+        {
+            return _filtroService.ObterPorParametros(parameters);
+        }
+
+        //[HttpGet("{codAcao}")]
+        //public Acao Get(int codAcao)
+        //{
+        //    return _filtroService.ObterPorCodigo(codAcao);
+        //}
+
+        [HttpPost]
+        public void Post([FromBody] FiltroUsuario filtroUsuario)
+        {
+            _filtroService.Criar(filtroUsuario);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] FiltroUsuario filtroUsuario)
+        {
+            _filtroService.Atualizar(filtroUsuario);
+        }
+
+        [HttpDelete("{codFiltroUsuario}")]
+        public void Delete(int codFiltroUsuario)
+        {
+            _filtroService.Deletar(codFiltroUsuario);
+        }
+    }
+}

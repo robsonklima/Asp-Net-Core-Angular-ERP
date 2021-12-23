15abc59c7b6cc753b4bd5b82113b2cfe746fdc62
PERTO\marilia.santos <mpdsantos@inf.ufpel.edu.br>
2021-12-23 15:50:54 -0300

    ajuste equipamento contrato

--- SAT.FRONT/src/app/modules/main/ordem-servico/ordem-servico-form/ordem-servico-form.component.ts
+++ SAT.FRONT/src/app/modules/main/ordem-servico/ordem-servico-form/ordem-servico-form.component.ts
@@ -249,13 +249,17 @@ export class OrdemServicoFormComponent implements OnInit, OnDestroy
       pageSize: 100
     }).toPromise();
 
-    this.equipamentosContrato = data.items.slice();
+    this.equipamentosContrato = Enumerable.from(data.items)
+      .orderByDescending(i => i.indAtivo)
+      .thenBy(i => i.numSerie)
+      .toArray();
   }
 
   private obterEquipamentosAoTrocarLocal()
   {
     this.form.controls['codPosto'].valueChanges.subscribe(() => 
     {
+      this.form.controls['codEquipContrato'].setValue(null);
       this.obterEquipamentos();
     });
   }

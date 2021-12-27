6c051a13b24bca09b61565066929d26ca8265c60
robsonklima <robsonklima@gmail.com>
2021-12-27 08:31:30 -0300

    REF

--- SAT.FRONT/src/app/modules/main/configuracoes/conta/conta.component.ts
+++ SAT.FRONT/src/app/modules/main/configuracoes/conta/conta.component.ts
@@ -1,33 +1,35 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'app/core/types/usuario.types';
+import { UserService } from 'app/core/user/user.service';
+import { UserSession } from 'app/core/user/user.types';
 
 @Component({
-    selector       : 'configuracoes-conta',
-    templateUrl    : './conta.component.html',
-    encapsulation  : ViewEncapsulation.None,
+    selector: 'configuracoes-conta',
+    templateUrl: './conta.component.html',
+    encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ConfiguracoesContaComponent implements OnInit
-{
-    accountForm: FormGroup;
-    
+export class ConfiguracoesContaComponent implements OnInit {
+    form: FormGroup;
+    userSession: UserSession;
+    usuario: Usuario;
+
     constructor(
-        private _formBuilder: FormBuilder
-    ) {}
-    
-    ngOnInit(): void
-    {
-        // Create the form
-        this.accountForm = this._formBuilder.group({
-            nome: ['Robson Lima'],
-            codUsuario: ['rklima'],
-            title: ['Senior Frontend Developer'],
-            company: ['YXZ Software'],
-            about   : ['Hey! This is Brian; husband, father and gamer. I\'m mostly passionate about bleeding edge tech and chocolate! 🍫'],
-            email   : ['hughes.brian@mail.com', Validators.email],
-            phone   : ['121-490-33-12'],
-            country : ['usa'],
-            language: ['english']
+        private _formBuilder: FormBuilder,
+        private _userSvc: UserService
+    ) {
+        this.userSession = JSON.parse(this._userSvc.userSession);
+    }
+
+    async ngOnInit() {
+        this.usuario = await this._userSvc.obterPorCodigo(this.userSession.usuario.codUsuario).toPromise();
+
+        this.form = this._formBuilder.group({
+            nomeUsuario: this.usuario.nomeUsuario,
+            codUsuario: this.usuario.codUsuario,
+            email: ['hughes.brian@mail.com', Validators.email],
+            phone: ['121-490-33-12'],
         });
     }
 }

cb6ff08fb8c3773f3d8545b7688c3fc19c170d90
robsonklima <robsonklima@gmail.com>
2021-12-28 12:51:07 -0300

    routing

--- SAT.FRONT/src/app/modules/main/ajuda/ajuda.routing.ts
+++ SAT.FRONT/src/app/modules/main/ajuda/ajuda.routing.ts
@@ -3,7 +3,16 @@ import { AjudaFaqComponent } from './ajuda-faq/ajuda-faq.component';
 
 export const ajudaRoutes: Route[] = [
     {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'ajuda-faq'
+    },
+    {
         path: 'ajuda-faq',
         component: AjudaFaqComponent
-    }
+    },
+    {
+        path: 'ajuda-suporte',
+        component: AjudaFaqComponent
+    },
 ];

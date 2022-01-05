86eea34cfda36deb43f4a6759be3db0a9f5e4da5
PERTO\marilia.santos <mpdsantos@inf.ufpel.edu.br>
2022-01-05 15:05:40 -0300

    lista de material editavel parc

--- SAT.FRONT/src/app/core/interceptors/global-error.interceptor.ts
+++ SAT.FRONT/src/app/core/interceptors/global-error.interceptor.ts
@@ -5,18 +5,23 @@ import { appConfig as c } from 'app/core/config/app.config';
 import { Router } from "@angular/router";
 
 @Injectable()
-export class GlobalErrorInterceptor implements ErrorHandler {
-    constructor(
+export class GlobalErrorInterceptor implements ErrorHandler
+{
+    constructor (
         private _zone: NgZone,
         private _emailSvc: EmailService,
         private _router: Router
     ) { }
 
-    handleError(error: any) {
-        if (!(error instanceof HttpErrorResponse)) {
+    handleError(error: any)
+    {
+        if (!(error instanceof HttpErrorResponse) || !error)
+        {
             error = error.rejection;
+            return;
         }
-        this._zone.run(() => {
+        this._zone.run(() =>
+        {
             const err: any = {
                 type: 'CLIENT_SIDE',
                 stack: error?.stack,
@@ -24,20 +29,20 @@ export class GlobalErrorInterceptor implements ErrorHandler {
                 status: error?.status
             }
 
-            if (err.status !== 401 && err.status !== 0) {
-                this._emailSvc.enviarEmail({
-                    nomeRemetente: c.system_user,
-                    emailRemetente: c.email_equipe,
-                    nomeDestinatario: c.system_user,
-                    emailDestinatario: c.email_equipe,
-                    assunto: 'Erro durante o uso do SAT.V2: FRONTEND',
-                    corpo: `Tipo: ${err.type}\n Mensagem: ${err.message}\n Stack: ${err.stack}`
-                }).toPromise();
+            //             if (err.status !== 401 && err.status !== 0) {
+            //                 this._emailSvc.enviarEmail({
+            //                     nomeRemetente: c.system_user,
+            //                     emailRemetente: c.email_equipe,
+            //                     nomeDestinatario: c.system_user,
+            //                     emailDestinatario: c.email_equipe,
+            //                     assunto: 'Erro durante o uso do SAT.V2: FRONTEND',
+            //                     corpo: `Tipo: ${err.type}\n Mensagem: ${err.message}\n Stack: ${err.stack}`
+            //                 }).toPromise();
+            // 
+            //                 this._router.navigate(['500-internal-server-error']);
+            //             }
 
-                this._router.navigate(['500-internal-server-error']);
-            }
-
-            console.log('Ocorreu um erro',  error);
+            console.log('Ocorreu um erro', error);
         });
     }
 }
--- SAT.FRONT/src/app/modules/main/orcamento/orcamento-detalhe/orcamento-detalhe-material/orcamento-detalhe-material.component.ts
+++ SAT.FRONT/src/app/modules/main/orcamento/orcamento-detalhe/orcamento-detalhe-material/orcamento-detalhe-material.component.ts
@@ -1,41 +1,99 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { fuseAnimations } from '@fuse/animations';
 import { OrcamentoMaterial } from 'app/core/types/orcamento.types';
+import { UserService } from 'app/core/user/user.service';
+import { IEditableItem, IEditableItemList } from 'app/shared/components/interfaces/ieditable-item-list';
 import { ConfirmacaoDialogComponent } from 'app/shared/confirmacao-dialog/confirmacao-dialog.component';
+import Enumerable from 'linq';
+import { isEqual } from 'lodash';
 
 @Component({
   selector: 'app-orcamento-detalhe-material',
   templateUrl: './orcamento-detalhe-material.component.html',
   styles: [`
         .list-grid-material {
-            grid-template-columns: 100px auto 100px 75px 100px 100px 100px 100px;
+            grid-template-columns: 100px auto 100px 75px 100px 100px 100px 150px;
             
             @screen sm {
-                grid-template-columns: 100px auto 100px 75px 100px 100px 100px 100px;
+                grid-template-columns: 100px auto 100px 75px 100px 100px 100px 150px;
             }
         }
     `],
   encapsulation: ViewEncapsulation.None,
   animations: fuseAnimations
 })
-export class OrcamentoDetalheMaterialComponent implements OnInit
+export class OrcamentoDetalheMaterialComponent implements IEditableItemList, AfterViewInit
 {
+  isLoading: boolean = false;
+  isEditing: boolean = false;
+  editableList: IEditableItem[] = [];
 
-  codOrc: number;
-  isLoading: boolean;
   @Input() materiais: OrcamentoMaterial[];
 
-  constructor (public _dialog: MatDialog) { }
+  constructor (public _dialog: MatDialog, private _cdRef: ChangeDetectorRef, private _userService: UserService) { }
 
-  ngOnInit(): void { }
+  ngAfterViewInit(): void
+  {
+    this.createEditableList();
+  }
 
-  excluirMaterial(material: OrcamentoMaterial) 
+  createEditableList()
+  {
+    Enumerable.from(this.materiais).forEach(i => 
+    {
+      var item: IEditableItem =
+      {
+        item: i,
+        oldItem: Object.assign({}, i),
+        isEditing: false
+      };
+
+      this.editableList.push(item);
+    });
+  }
+
+  editar(material: IEditableItem): void
+  {
+    if (!material) return;
+    this.isEditing = true;
+    material.isEditing = true;
+  }
+
+  salvar(material: IEditableItem): void
+  {
+    if (!material) return;
+    this.isEditing = false;
+    material.isEditing = false;
+  }
+
+  cancelar(material: IEditableItem): void
+  {
+    if (!material) return;
+
+    material.item = Object.assign({}, material.oldItem);
+    this.isEditing = false;
+    material.isEditing = false;
+    this._cdRef.detectChanges();
+  }
+
+  isEqual(material: IEditableItem): boolean
+  {
+    if (!material) return;
+    return isEqual(material.oldItem, material.item);
+  }
+
+  toNumber(value)
+  {
+    return +value;
+  }
+
+  excluirMaterial(material: IEditableItem) 
   {
     const dialogRef = this._dialog.open(ConfirmacaoDialogComponent, {
       data: {
         titulo: 'Confirmação',
-        message: `Deseja remover o material ${material.codigoMagnus}?`,
+        message: `Deseja remover o material ${material?.item.codigoMagnus}?`,
         buttonText: {
           ok: 'Sim',
           cancel: 'Não'

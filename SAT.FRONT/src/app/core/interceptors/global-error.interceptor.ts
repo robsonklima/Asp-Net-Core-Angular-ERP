22a83285e679df581aed8c9ea6fa70e500451841
PERTO\marilia.santos <mpdsantos@inf.ufpel.edu.br>
2022-01-05 15:09:06 -0300

    ref interceptor

--- SAT.FRONT/src/app/core/interceptors/global-error.interceptor.ts
+++ SAT.FRONT/src/app/core/interceptors/global-error.interceptor.ts
@@ -29,18 +29,19 @@ export class GlobalErrorInterceptor implements ErrorHandler
                 status: error?.status
             }
 
-            //             if (err.status !== 401 && err.status !== 0) {
-            //                 this._emailSvc.enviarEmail({
-            //                     nomeRemetente: c.system_user,
-            //                     emailRemetente: c.email_equipe,
-            //                     nomeDestinatario: c.system_user,
-            //                     emailDestinatario: c.email_equipe,
-            //                     assunto: 'Erro durante o uso do SAT.V2: FRONTEND',
-            //                     corpo: `Tipo: ${err.type}\n Mensagem: ${err.message}\n Stack: ${err.stack}`
-            //                 }).toPromise();
-            // 
-            //                 this._router.navigate(['500-internal-server-error']);
-            //             }
+            if (err.status !== 401 && err.status !== 0)
+            {
+                this._emailSvc.enviarEmail({
+                    nomeRemetente: c.system_user,
+                    emailRemetente: c.email_equipe,
+                    nomeDestinatario: c.system_user,
+                    emailDestinatario: c.email_equipe,
+                    assunto: 'Erro durante o uso do SAT.V2: FRONTEND',
+                    corpo: `Tipo: ${err.type}\n Mensagem: ${err.message}\n Stack: ${err.stack}`
+                }).toPromise();
+
+                this._router.navigate(['500-internal-server-error']);
+            }
 
             console.log('Ocorreu um erro', error);
         });

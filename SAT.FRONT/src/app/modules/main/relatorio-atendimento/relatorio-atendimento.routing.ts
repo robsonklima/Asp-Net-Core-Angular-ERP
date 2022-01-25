f8bd11936fa72593fa6300daf7794f6b5f5a5630
robsonklima <robsonklima@gmail.com>
2022-01-25 08:05:50 -0300

    REF

--- SAT.FRONT/src/app/modules/main/relatorio-atendimento/relatorio-atendimento.routing.ts
+++ SAT.FRONT/src/app/modules/main/relatorio-atendimento/relatorio-atendimento.routing.ts
@@ -1,5 +1,4 @@
 import { Route } from '@angular/router';
-import { RelatorioAtendimentoDeslocamentoComponent } from './relatorio-atendimento-deslocamento/relatorio-atendimento-deslocamento.component';
 import { RelatorioAtendimentoFormComponent } from './relatorio-atendimento-form/relatorio-atendimento-form.component';
 import { RelatorioAtendimentoLaudoImpressaoComponent } from './relatorio-atendimento-laudo-impressao/relatorio-atendimento-laudo-impressao.component';
 
@@ -19,9 +18,5 @@ export const relatorioAtendimentoRoutes: Route[] = [
     {
         path: 'form/:codOS/:codRAT/impressao-laudo/:codLaudo',
         component: RelatorioAtendimentoLaudoImpressaoComponent
-    },
-    {
-        path: 'deslocamento',
-        component: RelatorioAtendimentoDeslocamentoComponent
     }
 ];

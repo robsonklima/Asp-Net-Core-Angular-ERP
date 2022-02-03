515a08e5691d5479db8db75a0c1d13108dc3eb95
Majerkowski <endriumajerkowski@gmail.com>
2022-02-03 16:05:21 -0300

    Ordenação do exportar

--- SAT.MODELS/ViewModels/OrdemServicoExcelViewModel.cs
+++ SAT.MODELS/ViewModels/OrdemServicoExcelViewModel.cs
@@ -10,15 +10,16 @@
         public string Status { get; set; }
         public string Intervencao { get; set; }
         public string Tecnico { get; set; }
-        public string Defeito { get; set; }
-        public int? PA { get; set; }
-        public string Local { get; set; }
-        public string Regiao { get; set; }
-        public string Autorizada { get; set; }
         public string NumBanco { get; set; }
-        public string SLA { get; set; }
+        public string NumAgencia { get; set; }
+        public string Local { get; set; }
         public string Equipamento { get; set; }
         public string Serie { get; set; }
+        public string SLA { get; set; }
+        public int? PA { get; set; }
+        public string Regiao { get; set; }
+        public string Defeito { get; set; }
+        public string Autorizada { get; set; }
         public int? Reincidencia { get; set; }
     }
 }

96e149cae6601fbca7c1931111c9554464988605
robsonklima <robsonklima@gmail.com>
2022-03-08 10:12:18 -0300

    REF

--- SAT.FRONT/src/app/core/config/app.config.types.ts
+++ SAT.FRONT/src/app/core/config/app.config.types.ts
@@ -1,5 +1,5 @@
 export const apiConst = {
 	LOCALHOST_44341: 'https://localhost:44341/api',
-    LOCALHOST_5001: 'https://localhost:5001/api',
+    LOCALHOST_05001: 'https://localhost:5001/api',
     PROD: 'https://sat.perto.com.br/SAT.V2.API/api'
 }

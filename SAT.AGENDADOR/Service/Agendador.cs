4e36124fb9226cd004bdcfd22918e351149ed8a9
robsonklima <robsonklima@gmail.com>
2022-03-28 14:14:37 -0300

    REF

--- SAT.AGENDADOR/Service/Agendador.cs
+++ SAT.AGENDADOR/Service/Agendador.cs
@@ -6,21 +6,13 @@ namespace SAT.AGENDADOR
 {
     public class Agendador
     {
-        // variavel estatica de Timer
         private static Timer agendamentoTimer;
 
-        /// <summary>
-        /// Inicializa uma tarefa/função para ser chamada em um intervalo de tempo
-        /// </summary>
-        /// <param name="task">tarefa/função a ser chamada</param>
-        /// <param name="intervaloEmSegundos">intervalo de tempo em segudos</param>
         protected static void AgendarTarefa(string nome, Action tarefa, double intervaloEmSegundos)
         {
-            // Cria um temporizador com o valor de intervalo em segundos
             agendamentoTimer = new();
             agendamentoTimer.Interval = intervaloEmSegundos * 1000;
 
-            // Seta a task para o cronômetro. 
             agendamentoTimer.Elapsed += delegate (Object source, ElapsedEventArgs e)
             {
                 try
@@ -38,10 +30,7 @@ namespace SAT.AGENDADOR
                 };
             };
 
-            // Faz o cronômetro disparar eventos repetidos
             agendamentoTimer.AutoReset = true;
-
-            // Inicializa o timer
             agendamentoTimer.Enabled = true;
         }
     }
--- SAT.AGENDADOR/Service/AgendamentoService.cs
+++ SAT.AGENDADOR/Service/AgendamentoService.cs
@@ -4,31 +4,21 @@ namespace SAT.AGENDADOR.Service
 {
     public class AgendamentoService : Agendador
     {
-        /// <summary>
-        /// Instancia dos serviços
-        /// </summary>
         private static Startup services;
 
-        // Inicializador
         public AgendamentoService()
         {
             services = new Startup();
             InicializaAgendamentos();
         }
 
-        /// <summary>
-        /// Inicializa os agendamentos
-        /// </summary>
         private static void InicializaAgendamentos()
         {
-            // Inicializa os agendamentos
             DateTime agora = DateTime.Now;
 
-            // Tarefa 1:
-            AgendarTarefa(
-            nome: "Cria pontos no Agenda Técnico",
-            tarefa: () => services.AgendaTecnicoService.CriaIntervalosDoDia(),
-            intervaloEmSegundos: 30 * 60); // 30 minutos
+            AgendarTarefa(nome: "Cria pontos no Agenda Técnico",
+                          tarefa: () => services.AgendaTecnicoService.CriaIntervalosDoDia(),
+                          intervaloEmSegundos: 30 * 60);
         }
     }
 }

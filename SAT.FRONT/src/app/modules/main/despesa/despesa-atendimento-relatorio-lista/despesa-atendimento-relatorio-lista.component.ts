fbc261f87438fd45fa4618039e71f2a056dee896
gabrielmandel <gabrielmandel51@gmail.com>
2022-04-20 14:34:49 -0300

    Nova coluna para total de Kms na tela de despesas em um período de um técnico escolhido

--- SAT.FRONT/src/app/modules/main/despesa/despesa-atendimento-relatorio-lista/despesa-atendimento-relatorio-lista.component.ts
+++ SAT.FRONT/src/app/modules/main/despesa/despesa-atendimento-relatorio-lista/despesa-atendimento-relatorio-lista.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, LOCALE_ID, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, LOCALE_ID, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MatSort } from '@angular/material/sort';
@@ -10,7 +10,7 @@ import { DespesaService } from 'app/core/services/despesa.service';
 import { OrdemServicoService } from 'app/core/services/ordem-servico.service';
 import { RelatorioAtendimentoService } from 'app/core/services/relatorio-atendimento.service';
 import { DespesaPeriodo } from 'app/core/types/despesa-periodo.types';
-import { DespesaData } from 'app/core/types/despesa.types';
+import { DespesaData, DespesaTipoEnum } from 'app/core/types/despesa.types';
 import { IFilterable } from 'app/core/types/filtro.types';
 import { OrdemServicoData } from 'app/core/types/ordem-servico.types';
 import { RelatorioAtendimentoData } from 'app/core/types/relatorio-atendimento.types';
@@ -19,170 +19,162 @@ import Enumerable from 'linq';
 import moment from 'moment';
 
 @Component({
-  selector: 'app-despesa-atendimento-relatorio-lista',
-  templateUrl: './despesa-atendimento-relatorio-lista.component.html',
-  styles: [`
+	selector: 'app-despesa-atendimento-relatorio-lista',
+	templateUrl: './despesa-atendimento-relatorio-lista.component.html',
+	styles: [`
         .list-grid-despesa-atendimento-relatorio {
-            grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px;
-            @screen sm { grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px; }
-            @screen md { grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px; }
-            @screen lg { grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px; }
+            grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px 100px;
+            /* @screen sm { grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px 100px; }
+            @screen md { grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px 100px; }
+            @screen lg { grid-template-columns: 60px 72px 72px 70px auto 75px 80px 75px 80px 100px 100px; } */
         }
     `],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations,
-  providers: [{ provide: LOCALE_ID, useValue: "pt-BR" }]
+	encapsulation: ViewEncapsulation.None,
+	animations: fuseAnimations,
+	providers: [{ provide: LOCALE_ID, useValue: "pt-BR" }]
 })
 
-export class DespesaAtendimentoRelatorioListaComponent extends Filterable implements AfterViewInit, IFilterable
-{
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild('sidenav') sidenav: MatSidenav;
+export class DespesaAtendimentoRelatorioListaComponent extends Filterable implements OnInit, IFilterable {
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild('sidenav') sidenav: MatSidenav;
 
-  isLoading: boolean = false;
-  periodo: DespesaPeriodo;
-  despesas: DespesaData;
-  rats: RelatorioAtendimentoData;
-  ordemServico: OrdemServicoData;
-  codTecnico: string;
+	isLoading: boolean = false;
+	periodo: DespesaPeriodo;
+	despesas: DespesaData;
+	rats: RelatorioAtendimentoData;
+	ordemServico: OrdemServicoData;
+	codTecnico: string;
 
-  constructor (
-    protected _userService: UserService,
-    private _cdr: ChangeDetectorRef,
-    private _route: ActivatedRoute,
-    private _despesaPeriodoSvc: DespesaPeriodoService,
-    private _relatorioAtendimentoSvc: RelatorioAtendimentoService,
-    private _despesaSvc: DespesaService,
-    private _ordemServicoSvc: OrdemServicoService)
-  {
-    super(_userService, "despesa-atendimento-relatorio");
-    this.codTecnico = this._route.snapshot.paramMap.get('codTecnico') || this.userSession.usuario?.codTecnico;
-  }
+	constructor(
+		protected _userService: UserService,
+		private _cdr: ChangeDetectorRef,
+		private _route: ActivatedRoute,
+		private _despesaPeriodoSvc: DespesaPeriodoService,
+		private _relatorioAtendimentoSvc: RelatorioAtendimentoService,
+		private _despesaSvc: DespesaService,
+		private _ordemServicoSvc: OrdemServicoService) {
+		super(_userService, "despesa-atendimento-relatorio");
+		this.codTecnico = this._route.snapshot.paramMap.get('codTecnico') || this.userSession.usuario?.codTecnico;
+	}
 
-  async ngAfterViewInit()
-  {
-    await this.obterDados();
+	async ngOnInit() {
+		await this.obterDados();
 
-    if (this.sort && this.paginator)
-    {
-      this.sort.disableClear = true;
-      this._cdr.markForCheck();
+		if (this.sort && this.paginator) {
+			this.sort.disableClear = true;
+			this._cdr.markForCheck();
 
-      this.sort.sortChange.subscribe(() =>
-      {
-        this.onSortChanged()
-        this.obterDados();
-      });
-    }
+			this.sort.sortChange.subscribe(() => {
+				this.onSortChanged()
+				this.obterDados();
+			});
+		}
 
-    this.registerEmitters();
-    this._cdr.detectChanges();
-  }
+		this.registerEmitters();
+		this._cdr.detectChanges();
+	}
 
-  private async obterPeriodo()
-  {
-    var codDespesaPeriodo =
-      +this._route.snapshot.paramMap.get('codDespesaPeriodo');
+	private async obterPeriodo() {
+		var codDespesaPeriodo =
+			+this._route.snapshot.paramMap.get('codDespesaPeriodo');
 
-    this.periodo =
-      (await this._despesaPeriodoSvc.obterPorCodigo(codDespesaPeriodo).toPromise());
-  }
+		this.periodo =
+			(await this._despesaPeriodoSvc.obterPorCodigo(codDespesaPeriodo).toPromise());
+	}
 
-  private async obterRATs()
-  {
-    this.rats = this.codTecnico != null ?
-      (await this._relatorioAtendimentoSvc.obterPorParametros
-        ({
-          codTecnicos: this.codTecnico,
-          dataInicio: moment(this.periodo.dataInicio).format('yyyy-MM-DD HH:mm:ss'),
-          dataSolucao: moment(this.periodo.dataFim).format('yyyy-MM-DD HH:mm:ss')
-        }).toPromise()) : null;
-  }
+	private async obterRATs() {
+		this.rats = this.codTecnico != null ?
+			(await this._relatorioAtendimentoSvc.obterPorParametros
+				({
+					codTecnicos: this.codTecnico,
+					dataInicio: moment(this.periodo.dataInicio).format('yyyy-MM-DD HH:mm:ss'),
+					dataSolucao: moment(this.periodo.dataFim).format('yyyy-MM-DD HH:mm:ss')
+				}).toPromise()) : null;
+	}
 
-  private async obterDespesas()
-  {
-    var codigos: string =
-      Enumerable.from(this.rats.items)
-        .select(i => i.codRAT)
-        .distinct()
-        .toJoinedString(',');
+	private async obterDespesas() {
+		var codigos: string =
+			Enumerable.from(this.rats.items)
+				.select(i => i.codRAT)
+				.distinct()
+				.toJoinedString(',');
 
-    this.despesas = this.codTecnico != null && codigos?.length > 0 ?
-      (await this._despesaSvc.obterPorParametros
-        ({
-          codRATs: codigos,
-          codTecnico: this.codTecnico
-        }).toPromise()) : null;
-  }
+		this.despesas = this.codTecnico != null && codigos?.length > 0 ?
+			(await this._despesaSvc.obterPorParametros
+				({
+					codRATs: codigos,
+					codTecnico: this.codTecnico
+				}).toPromise()) : null;
+	}
 
-  private async obterOrdensDeServico()
-  {
-    var codigos: string =
-      Enumerable.from(this.rats.items)
-        .select(i => i.codOS)
-        .distinct()
-        .toJoinedString(',');
+	private async obterOrdensDeServico() {
+		var codigos: string =
+			Enumerable.from(this.rats.items)
+				.select(i => i.codOS)
+				.distinct()
+				.toJoinedString(',');
 
-    this.ordemServico = (await this._ordemServicoSvc.obterPorParametros
-      ({ codOS: codigos, pageSize: codigos?.length > 0 ? codigos?.length : 1 }).toPromise());
-  }
+		this.ordemServico = (await this._ordemServicoSvc.obterPorParametros
+			({ codOS: codigos, pageSize: codigos?.length > 0 ? codigos?.length : 1 }).toPromise());
+	}
 
-  public async obterDados()
-  {
-    this.isLoading = true;
+	public async obterDados() {
+		this.isLoading = true;
 
-    await this.obterPeriodo();
-    await this.obterRATs();
-    await this.obterOrdensDeServico();
-    await this.obterDespesas();
+		await this.obterPeriodo();
+		await this.obterRATs();
+		await this.obterOrdensDeServico();
+		await this.obterDespesas();
 
-    this.isLoading = false;
-  }
+		this.isLoading = false;
+	}
 
-  registerEmitters(): void
-  {
-    this.sidenav?.closedStart.subscribe(() =>
-    {
-      this.onSidenavClosed();
-      this.obterDados();
-    })
-  }
+	registerEmitters(): void {
+		this.sidenav?.closedStart.subscribe(() => {
+			this.onSidenavClosed();
+			this.obterDados();
+		})
+	}
 
-  public paginar()
-  {
-    this.onPaginationChanged();
-    this.obterDados();
-  }
+	public paginar() {
+		this.onPaginationChanged();
+		this.obterDados();
+	}
 
-  public obterOSCliente(codOS?: number)
-  {
-    if (!codOS) return null;
-    return Enumerable.from(this.ordemServico?.items)
-      .firstOrDefault(i => i.codOS == codOS)?.numOSCliente;
-  }
+	public obterOSCliente(codOS?: number) {
+		if (!codOS) return null;
+		return Enumerable.from(this.ordemServico?.items)
+			.firstOrDefault(i => i.codOS == codOS)?.numOSCliente;
+	}
 
-  public obterNomeCliente(codOS?: number)
-  {
-    if (!codOS) return null;
-    return Enumerable.from(this.ordemServico?.items)
-      .firstOrDefault(i => i.codOS == codOS)?.cliente?.nomeFantasia;
-  }
+	public obterNomeCliente(codOS?: number) {
+		if (!codOS) return null;
+		return Enumerable.from(this.ordemServico?.items)
+			.firstOrDefault(i => i.codOS == codOS)?.cliente?.nomeFantasia;
+	}
 
-  public obterLocalAtendimento(codOS?: number)
-  {
-    if (!codOS) return null;
-    return Enumerable.from(this.ordemServico?.items)
-      .firstOrDefault(i => i.codOS == codOS)?.localAtendimento?.nomeLocal;
-  }
+	public obterLocalAtendimento(codOS?: number) {
+		if (!codOS) return null;
+		return Enumerable.from(this.ordemServico?.items)
+			.firstOrDefault(i => i.codOS == codOS)?.localAtendimento?.nomeLocal;
+	}
 
-  public obterTotalDespesa(codRAT?: number)
-  {
-    if (!codRAT) return null;
+	public obterTotalDespesa(codRAT?: number) {
+		if (!codRAT) return null;
 
-    return Enumerable.from(this.despesas.items)
-      .where(d => d.codRAT == codRAT)
-      .sum(d => Enumerable.from(d.despesaItens)
-        .sum(di => di.despesaValor));
-  }
+		return Enumerable.from(this.despesas.items)
+			.where(d => d.codRAT == codRAT)
+			.sum(d => Enumerable.from(d.despesaItens)
+				.sum(di => di.despesaValor));
+	}
+
+	public obterTotalKm(codRAT?: number) {
+			
+		return Enumerable.from(this.despesas.items)
+			.where(d => d.codRAT == codRAT)
+			.sum(d => Enumerable.from(d.despesaItens)
+				.where(di => di.codDespesaTipo == DespesaTipoEnum.KM)
+				.sum(di => di.kmPercorrido));
+	}
 }

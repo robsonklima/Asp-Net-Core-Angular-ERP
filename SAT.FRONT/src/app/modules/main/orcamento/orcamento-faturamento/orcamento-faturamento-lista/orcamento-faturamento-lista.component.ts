85e570d48661e75b38f181474e675b133ac4d2f6
gabrielmandel <gabrielmandel51@gmail.com>
2022-07-26 16:19:30 -0300

    Correção na exibição de informações para Dados de Locais para orçamento. Inserção de botão para redirecionar para os locais de faturamento corretos para o orçamento. Correção de patch value no codContrato do formulário

--- SAT.FRONT/src/app/modules/main/orcamento/orcamento-faturamento/orcamento-faturamento-lista/orcamento-faturamento-lista.component.ts
+++ SAT.FRONT/src/app/modules/main/orcamento/orcamento-faturamento/orcamento-faturamento-lista/orcamento-faturamento-lista.component.ts
@@ -7,13 +7,13 @@ import { LocalEnvioNFFaturamento, LocalEnvioNFFaturamentoData } from 'app/core/t
 import { UserSession } from 'app/core/user/user.types';
 
 @Component({
-  selector: 'app-orcamento-faturamento-lista',
-  templateUrl: './orcamento-faturamento-lista.component.html',
-  styles: [`
+	selector: 'app-orcamento-faturamento-lista',
+	templateUrl: './orcamento-faturamento-lista.component.html',
+	styles: [`
     .list-grid-faturamentos {
-      grid-template-columns: 72px 155px auto 155px 155px 72px 155px 72px;
+      grid-template-columns: 60px 155px auto 200px 150px 150px 160px 150px;
       
-      @screen sm {
+      /* @screen sm {
           grid-template-columns: 72px 155px auto 155px 155px 72px 155px 72px;
       }
 
@@ -23,61 +23,61 @@ import { UserSession } from 'app/core/user/user.types';
 
       @screen lg {
           grid-template-columns: 72px 155px auto 155px 155px 72px 155px 72px;
-      }
+      } */
     }  
   `],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations
+	encapsulation: ViewEncapsulation.None,
+	animations: fuseAnimations
 })
 export class OrcamentoFaturamentoListaComponent implements AfterViewInit {
-  [x: string]: any;
+	[x: string]: any;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) private sort: MatSort;
-  dataSourceData: LocalEnvioNFFaturamentoData;
-  isLoading: boolean = false;
-  @ViewChild('searchInputControl', { static: true }) searchInputControl: ElementRef;
-  selectedItem: LocalEnvioNFFaturamento | null = null;
-  userSession: UserSession;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) private sort: MatSort;
+	dataSourceData: LocalEnvioNFFaturamentoData;
+	isLoading: boolean = false;
+	@ViewChild('searchInputControl', { static: true }) searchInputControl: ElementRef;
+	selectedItem: LocalEnvioNFFaturamento | null = null;
+	userSession: UserSession;
 
-  constructor(
-    private _cdr: ChangeDetectorRef,
-    private _localEnvioNFFaturamentoService: LocalEnvioNFFaturamentoService
-  ) { }
+	constructor(
+		private _cdr: ChangeDetectorRef,
+		private _localEnvioNFFaturamentoService: LocalEnvioNFFaturamentoService
+	) { }
 
-  ngAfterViewInit() {
-    this.obterDados();
+	ngAfterViewInit() {
+		this.obterDados();
 
-    if (this.sort && this.paginator) {
-      this.sort.disableClear = true;
-      this._cdr.markForCheck();
+		if (this.sort && this.paginator) {
+			this.sort.disableClear = true;
+			this._cdr.markForCheck();
 
-      this.sort.sortChange.subscribe(() => {
-        this.onSortChanged()
-        this.obterDados();
-      });
-    }
+			this.sort.sortChange.subscribe(() => {
+				this.onSortChanged()
+				this.obterDados();
+			});
+		}
 
-    this.registerEmitters();
-    this._cdr.detectChanges();
-  }
+		this.registerEmitters();
+		this._cdr.detectChanges();
+	}
 
-  obterDados(): void {
-    this.isLoading = true;
-    this._localEnvioNFFaturamentoService.obterPorParametros({
-      pageNumber: this.paginator?.pageIndex + 1,
-      sortActive: this.sort.active,
-      sortDirection: this.sort.direction,
-      pageSize: this.paginator?.pageSize,
-      filter: this.searchInputControl.nativeElement.val
-    }).subscribe((data: LocalEnvioNFFaturamentoData) => {
-      this.dataSourceData = data;
-      this.isLoading = false;
-      this._cdr.detectChanges();
-    });
-  }
+	obterDados(): void {
+		this.isLoading = true;
+		this._localEnvioNFFaturamentoService.obterPorParametros({
+			pageNumber: this.paginator?.pageIndex + 1,
+			sortActive: this.sort.active,
+			sortDirection: this.sort.direction,
+			pageSize: this.paginator?.pageSize,
+			filter: this.searchInputControl.nativeElement.val
+		}).subscribe((data: LocalEnvioNFFaturamentoData) => {
+			this.dataSourceData = data;
+			this.isLoading = false;
+			this._cdr.detectChanges();
+		});
+	}
 
-  paginar() {
-    this.obterDados();
-  }
+	paginar() {
+		this.obterDados();
+	}
 }

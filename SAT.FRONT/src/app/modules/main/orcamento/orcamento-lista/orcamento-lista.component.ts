a4a1a69892351b7b970541b976eb03d5857acc8b
gabrielmandel <gabrielmandel51@gmail.com>
2022-07-29 08:53:04 -0300

    Regras de cores para exibição dos tipos de intervenção da tela de orçamentos lista

--- SAT.FRONT/src/app/modules/main/orcamento/orcamento-lista/orcamento-lista.component.ts
+++ SAT.FRONT/src/app/modules/main/orcamento/orcamento-lista/orcamento-lista.component.ts
@@ -6,126 +6,139 @@ import { fuseAnimations } from '@fuse/animations';
 import { Filterable } from 'app/core/filters/filterable';
 import { OrcamentoService } from 'app/core/services/orcamento.service';
 import { IFilterable } from 'app/core/types/filtro.types';
-import { Orcamento, OrcamentoData, OrcamentoParameters } from 'app/core/types/orcamento.types';
+import { Orcamento, OrcamentoData, OrcamentoParameters, OrcamentoTipoIntervencao } from 'app/core/types/orcamento.types';
 import { UserService } from 'app/core/user/user.service';
 import { fromEvent, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-orcamento-lista',
-  templateUrl: './orcamento-lista.component.html',
-  styles: [`
+	selector: 'app-orcamento-lista',
+	templateUrl: './orcamento-lista.component.html',
+	styles: [`
         .list-grid-orcamentos {
-            grid-template-columns: 48px 72px 48px 80px 118px 118px auto 72px 48px 120px 248px 70px 120px;
+            grid-template-columns: 48px 72px 48px 80px 118px 118px auto 250px 30px 70px 200px 100px 100px;
             
-            @screen md {
+            /* @screen md {
               grid-template-columns: 48px 72px 48px 80px 118px 72px auto 148px 48px 120px;
             }
 
             @screen lg {
               grid-template-columns: 48px 72px 48px 80px 118px 118px auto 72px 48px 120px 148px 70px 120px;
-          }
+          } */
         }
     `],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations
+	encapsulation: ViewEncapsulation.None,
+	animations: fuseAnimations
 })
-export class OrcamentoListaComponent extends Filterable implements AfterViewInit, IFilterable
-{
-  @ViewChild('sidenav') sidenav: MatSidenav;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild('searchInputControl') searchInputControl: ElementRef;
+export class OrcamentoListaComponent extends Filterable implements AfterViewInit, IFilterable {
+	@ViewChild('sidenav') sidenav: MatSidenav;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild('searchInputControl') searchInputControl: ElementRef;
 
-  @ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatSort) sort: MatSort;
 
-  dataSourceData: OrcamentoData;
-  selectedItem: Orcamento | null = null;
-  isLoading: boolean = false;
-  protected _onDestroy = new Subject<void>();
+	dataSourceData: OrcamentoData;
+	selectedItem: Orcamento | null = null;
+	isLoading: boolean = false;
+	protected _onDestroy = new Subject<void>();
 
-  constructor (
-    private _orcamentoSvc: OrcamentoService,
-    private _cdr: ChangeDetectorRef,
-    protected _userService: UserService
-  )
-  {
-    super(_userService, 'orcamento');
-  }
+	constructor(
+		private _orcamentoSvc: OrcamentoService,
+		private _cdr: ChangeDetectorRef,
+		protected _userService: UserService
+	) {
+		super(_userService, 'orcamento');
+	}
 
-  ngAfterViewInit(): void
-  {
-    this.obterOrcamentos();
-    this.registerEmitters();
+	ngAfterViewInit(): void {
+		this.obterOrcamentos();
+		this.registerEmitters();
 
-    fromEvent(this.searchInputControl.nativeElement, 'keyup').pipe(
-      map((event: any) =>
-      {
-        return event.target.value;
-      })
-      , debounceTime(1000)
-      , distinctUntilChanged()
-    ).subscribe((text: string) =>
-    {
-      this.paginator.pageIndex = 0;
-      this.obterOrcamentos(text);
-    });
+		fromEvent(this.searchInputControl.nativeElement, 'keyup').pipe(
+			map((event: any) => {
+				return event.target.value;
+			})
+			, debounceTime(1000)
+			, distinctUntilChanged()
+		).subscribe((text: string) => {
+			this.paginator.pageIndex = 0;
+			this.obterOrcamentos(text);
+		});
 
-    if (this.sort && this.paginator)
-    {
-      this.sort.disableClear = true;
-      this._cdr.markForCheck();
+		if (this.sort && this.paginator) {
+			this.sort.disableClear = true;
+			this._cdr.markForCheck();
 
-      this.sort.sortChange.subscribe(() =>
-      {
-        this.onSortChanged();
-        this.obterOrcamentos();
-      });
-    }
+			this.sort.sortChange.subscribe(() => {
+				this.onSortChanged();
+				this.obterOrcamentos();
+			});
+		}
 
-    this._cdr.detectChanges();
-  }
+		this._cdr.detectChanges();
+	}
 
-  registerEmitters(): void
-  {
-    this.sidenav.closedStart.subscribe(() =>
-    {
-      this.onSidenavClosed();
-      this.obterOrcamentos();
-    })
-  }
+	registerEmitters(): void {
+		this.sidenav.closedStart.subscribe(() => {
+			this.onSidenavClosed();
+			this.obterOrcamentos();
+		})
+	}
 
-  private async obterOrcamentos(filtro: string = '')
-  {
-    this.isLoading = true;
+	private async obterOrcamentos(filtro: string = '') {
+		this.isLoading = true;
 
-    const params: OrcamentoParameters = {
-      pageNumber: this.paginator.pageIndex + 1,
-      sortActive: this.filter?.parametros?.sortActive || this.sort.active || 'codOrc',
-      sortDirection: this.filter?.parametros?.direction || this.sort.direction || 'desc',
-      pageSize: this.filter?.parametros?.qtdPaginacaoLista ?? this.paginator?.pageSize,
-      filter: filtro
-    };
+		const params: OrcamentoParameters = {
+			pageNumber: this.paginator.pageIndex + 1,
+			sortActive: this.filter?.parametros?.sortActive || this.sort.active || 'codOrc',
+			sortDirection: this.filter?.parametros?.direction || this.sort.direction || 'desc',
+			pageSize: this.filter?.parametros?.qtdPaginacaoLista ?? this.paginator?.pageSize,
+			filter: filtro
+		};
 
-    const data: OrcamentoData = await this._orcamentoSvc
-      .obterPorParametros({
-        ...params,
-        ...this.filter?.parametros
-      })
-      .toPromise();
+		const data: OrcamentoData = await this._orcamentoSvc
+			.obterPorParametros({
+				...params,
+				...this.filter?.parametros
+			})
+			.toPromise();
 
-    this.dataSourceData = data;
-    this.isLoading = false;
-  }
+		this.dataSourceData = data;
+		this.isLoading = false;
+	}
 
-  paginar()
-  {
-    this.onPaginationChanged();
-    this.obterOrcamentos();
-  }
+	obterCorFundoIntervencao(codTipoIntervencao: number) {
+		switch (codTipoIntervencao) {
+			case OrcamentoTipoIntervencao.ORCAMENTO_APROVADO:
+				return 'blue'
+		
+			case OrcamentoTipoIntervencao.ORCAMENTO:
+				return 'pink'
+		
+			case OrcamentoTipoIntervencao.ORCAMENTO_REPROVADO:
+				return 'gray'
+		
+			default:
+				break;
+		}
+	}
 
-  ngOnDestroy()
-  {
-    this._onDestroy.next();
-    this._onDestroy.complete();
-  }
+	obterCorFonteIntervencao(codTipoIntervencao: number) {
+
+		if (codTipoIntervencao == OrcamentoTipoIntervencao.ORCAMENTO_APROVADO || 
+			codTipoIntervencao == OrcamentoTipoIntervencao.ORCAMENTO_REPROVADO) {
+			return 'white'
+		}
+
+	}
+
+	paginar() {
+		this.onPaginationChanged();
+		this.obterOrcamentos();
+	}
+
+	ngOnDestroy() {
+		this._onDestroy.next();
+		this._onDestroy.complete();
+	}
 }

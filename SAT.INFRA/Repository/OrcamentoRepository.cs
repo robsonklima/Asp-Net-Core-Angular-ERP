a699a209a0240282f1553c27c65c005b89d83b8f
gabrielmandel <gabrielmandel51@gmail.com>
2022-08-04 17:02:43 -0300

    REF

--- SAT.INFRA/Repository/OrcamentoRepository.cs
+++ SAT.INFRA/Repository/OrcamentoRepository.cs
@@ -91,7 +91,8 @@ namespace SAT.INFRA.Repository
                 .Include(p => p.OutrosServicos)
                 .Include(p => p.Descontos)
                 .Include(p => p.OrcamentoStatus)
-                .Include(p => p.OrcamentosFaturamento)
+                .Include(p => p.OrcamentosFaturamento!)
+                    .DefaultIfEmpty()
                 .Include(p => p.OrcamentoDeslocamento)
                 .FirstOrDefault(p => p.CodOrc == codigo);
         }

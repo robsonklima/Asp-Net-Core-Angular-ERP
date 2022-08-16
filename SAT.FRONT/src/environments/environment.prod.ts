9153ace425949644fa65e17dadc63a988af5ec6e
gabrielmandel <gabrielmandel51@gmail.com>
2022-08-16 09:43:50 -0300

    Inclusao de Link no envio do orçamento

--- SAT.FRONT/src/environments/environment.prod.ts
+++ SAT.FRONT/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://sat.perto.com.br/SAT.V2.API/api'
+    apiUrl: 'https://sat.perto.com.br/SAT.V2.API/api',
+    clientUrl: 'https://sat.perto.com.br/SAT.V2.FRONTEND'
 };

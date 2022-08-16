050def990a8744508cf1ef069d9cf726b004af55
PERTO\johnney.souza <johnney.souza@perto.com.br>
2022-08-16 16:41:27 -0300

    Merge branch 'development' of https://github.com/robsonklima/SAT.V2 into development CRUD - Auditoria Fotos e Status

---
+++ SAT.SERVICES/Services/AuditoriaStatusService.cs
@@ -0,0 +1,55 @@
+﻿using SAT.INFRA.Interfaces;
+using SAT.MODELS.Entities;
+using SAT.MODELS.Entities.Params;
+using SAT.MODELS.ViewModels;
+using SAT.SERVICES.Interfaces;
+
+namespace SAT.SERVICES.Services
+{
+    public class AuditoriaStatusService : IAuditoriaStatusService
+    {
+        private readonly IAuditoriaStatusRepository _auditoriaStatusRepo;
+        public AuditoriaStatusService( IAuditoriaStatusRepository auditoriaStatusRepo ) 
+        {
+            _auditoriaStatusRepo = auditoriaStatusRepo;
+        }
+
+        public void Atualizar(AuditoriaStatus auditoriaStatus)
+        {
+            _auditoriaStatusRepo.Atualizar(auditoriaStatus);
+        }
+
+        public void Criar(AuditoriaStatus auditoriaStatus)
+        {
+            _auditoriaStatusRepo.Criar(auditoriaStatus);
+        }
+
+        public void Deletar(int codigoAuditoriaStatus)
+        {
+            _auditoriaStatusRepo.Deletar(codigoAuditoriaStatus);
+        }
+
+        public AuditoriaStatus ObterPorCodigo(int codAuditoriaStatus)
+        {
+            return _auditoriaStatusRepo.ObterPorCodigo(codAuditoriaStatus);
+        }
+
+        public ListViewModel ObterPorParametros(AuditoriaStatusParameters parameters)
+        {
+            var auditoriasStatus = _auditoriaStatusRepo.ObterPorParametros(parameters);
+
+            var lista = new ListViewModel
+            {
+                Items = auditoriasStatus,
+                TotalCount = auditoriasStatus.TotalCount,
+                CurrentPage = auditoriasStatus.CurrentPage,
+                PageSize = auditoriasStatus.PageSize,
+                TotalPages = auditoriasStatus.TotalPages,
+                HasNext = auditoriasStatus.HasNext,
+                HasPrevious = auditoriasStatus.HasPrevious
+            };
+
+            return lista;
+        }
+    }
+}

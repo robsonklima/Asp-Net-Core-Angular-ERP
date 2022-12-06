ec5d5eb7d8a6d7d6ceb025606a22aa21a90843d8
PERTO\johnney.souza <johnney.souza@perto.com.br>
2022-12-06 10:46:50 -0300

    REF

--- SAT.FRONT/src/app/modules/main/suporte-stn/suporte-stn-laudo/suporte-stn-laudo-form/suporte-stn-laudo-form-informacoes/suporte-stn-laudo-form-informacoes.component.ts
+++ SAT.FRONT/src/app/modules/main/suporte-stn/suporte-stn-laudo/suporte-stn-laudo-form/suporte-stn-laudo-form-informacoes/suporte-stn-laudo-form-informacoes.component.ts
@@ -7,6 +7,8 @@ import { Laudo } from 'app/core/types/laudo.types';
 import { LaudoService } from 'app/core/services/laudo.service';
 import { RelatorioAtendimento } from 'app/core/types/relatorio-atendimento.types';
 import { RelatorioAtendimentoService } from 'app/core/services/relatorio-atendimento.service';
+import { OrdemServico } from 'app/core/types/ordem-servico.types';
+import { OrdemServicoService } from 'app/core/services/ordem-servico.service';
 
 @Component({
     selector: 'app-suporte-stn-laudo-form-informacoes',
@@ -16,6 +18,7 @@ import { RelatorioAtendimentoService } from 'app/core/services/relatorio-atendim
 	
 	codLaudo: number;
 	laudo: Laudo;
+	or: OrdemServico;
     relatorioAtendimento: RelatorioAtendimento;
 	isLoading: boolean;
 	userSession: UsuarioSessao;
@@ -26,6 +29,7 @@ import { RelatorioAtendimentoService } from 'app/core/services/relatorio-atendim
 		private _userService: UserService,
 		private _laudoService: LaudoService,
         private _relatorioAtendimentoService: RelatorioAtendimentoService,
+		private _ordemServicoService: OrdemServicoService
 	) {
 		this.userSession = JSON.parse(this._userService.userSession);
 	}
@@ -34,6 +38,7 @@ import { RelatorioAtendimentoService } from 'app/core/services/relatorio-atendim
         this.codLaudo = +this._route.snapshot.paramMap.get('codLaudo');
         this.laudo = await this._laudoService.obterPorCodigo(this.codLaudo).toPromise();
         this.relatorioAtendimento = await this._relatorioAtendimentoService.obterPorCodigo(this.laudo.codRAT).toPromise();
+		this.or = await this._ordemServicoService.obterPorCodigo(this.laudo.codOS).toPromise();
 	}
 
     ngOnDestroy() {

068b2f1583b75de544c1ee3640803ee04c0dbaac
PERTO\johnney.souza <johnney.souza@perto.com.br>
2022-12-08 16:19:29 -0300

    REF

---
+++ SAT.FRONT/src/app/modules/main/suporte-stn/ordem-servico-stn-form/ordem-servico-stn-form-historico/ordem-servico-stn-form-historico.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordem-servico-stn-form-historico',
+  templateUrl: './ordem-servico-stn-form-historico.component.html'
+})
+export class OrdemServicoStnFormHistoricoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
--- SAT.FRONT/src/app/modules/main/suporte-stn/ordem-servico-stn-form/ordem-servico-stn-form-informacao/ordem-servico-stn-form-informacao.component.ts
+++ SAT.FRONT/src/app/modules/main/suporte-stn/ordem-servico-stn-form/ordem-servico-stn-form-informacao/ordem-servico-stn-form-informacao.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ordem-servico-stn-form-informacao',
   templateUrl: './ordem-servico-stn-form-informacao.component.html'
 })
 export class OrdemServicoStnFormInformacaoComponent implements OnInit {
-
+    @Input() codOS: number;
   constructor() { }
 
   ngOnInit(){}
+
+  salvar(){}
 }

3f2b318e17e88ef0fa5eefb86d5ad8eb70ba995b
PERTO\johnney.souza <johnney.souza@perto.com.br>
2022-12-13 15:41:09 -0300

    Implementadas as telas de histórico e atendimento do Suporte STN Form

--- SAT.FRONT/src/app/modules/main/suporte-stn/ordem-servico-stn-form/ordem-servico-stn-form-historico/ordem-servico-stn-form-historico.component.ts
+++ SAT.FRONT/src/app/modules/main/suporte-stn/ordem-servico-stn-form/ordem-servico-stn-form-historico/ordem-servico-stn-form-historico.component.ts
@@ -1,13 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { OrdemServicoSTNService } from 'app/core/services/ordem-servico-stn.service';
+import { StatusServicoSTNService } from 'app/core/services/status-servico-stn.service';
+import { OrdemServicoSTN } from 'app/core/types/ordem-servico-stn.types';
+import { StatusServicoSTN } from 'app/core/types/status-servico-stn.types';
+import { UsuarioSessao } from 'app/core/types/usuario.types';
+import { UserService } from 'app/core/user/user.service';
 
 @Component({
   selector: 'app-ordem-servico-stn-form-historico',
   templateUrl: './ordem-servico-stn-form-historico.component.html'
 })
 export class OrdemServicoStnFormHistoricoComponent implements OnInit {
-    @Input() codOS: number;
-  constructor() { }
+  @Input() codAtendimento: number;
+  atendimento: OrdemServicoSTN;
+  status: StatusServicoSTN;
+  userSession: UsuarioSessao;
 
-  ngOnInit(): void {
-  }
+constructor(
+  private _ordemServicoSTNService: OrdemServicoSTNService,
+  private _statusSTNService: StatusServicoSTNService,
+  private _cdr: ChangeDetectorRef,
+  private _userService: UserService,
+  private _dialog: MatDialog,
+  private _router: Router
+) {
+  this.userSession = JSON.parse(this._userService.userSession);
+}
+
+async ngOnInit(){
+  this.atendimento = await this._ordemServicoSTNService.obterPorCodigo(this.codAtendimento).toPromise();
+  this.status = await this._statusSTNService.obterPorCodigo(this.atendimento.codStatusSTN).toPromise();
+}
+
+onChange(){}
 }

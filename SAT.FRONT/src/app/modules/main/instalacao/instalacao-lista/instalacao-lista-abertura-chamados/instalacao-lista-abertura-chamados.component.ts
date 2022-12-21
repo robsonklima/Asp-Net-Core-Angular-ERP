be247b56dcb72074b942096b39cb2648de305c2d
ealmanca <ealmanca@gmail.com>
2022-12-21 10:23:45 -0300

    importacao

---
+++ SAT.FRONT/src/app/modules/main/instalacao/instalacao-lista/instalacao-lista-abertura-chamados/instalacao-lista-abertura-chamados.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instalacao-lista-abertura-chamados',
+  templateUrl: './instalacao-lista-abertura-chamados.component.html'
+})
+export class InstalacaoListaAberturaChamadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

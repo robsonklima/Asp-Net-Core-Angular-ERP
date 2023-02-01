f3c590d035bdc5634fe7e39a4a237c6a05d47549
PERTO\johnney.souza <johnney.souza@perto.com.br>
2023-02-01 15:01:06 -0300

    REF - Observaçoes Auditoria

--- SAT.FRONT/src/app/modules/main/frota-tecnicos/auditoria/auditoria-utilizacao/auditoria-utilizacao.component.ts
+++ SAT.FRONT/src/app/modules/main/frota-tecnicos/auditoria/auditoria-utilizacao/auditoria-utilizacao.component.ts
@@ -1,16 +1,16 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { UsuarioSessao } from 'app/core/types/usuario.types';
-import { UserService } from 'app/core/user/user.service';
-import { fromEvent, Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged, first, map } from 'rxjs/operators';
-import { Auditoria } from 'app/core/types/auditoria.types';
 import { AuditoriaService } from 'app/core/services/auditoria.service';
+import { CustomSnackbarService } from 'app/core/services/custom-snackbar.service';
 import { AuditoriaVeiculoTanque } from 'app/core/types/auditoria-veiculo-tanque.types';
+import { Auditoria } from 'app/core/types/auditoria.types';
 import { DespesaConfiguracaoCombustivel } from 'app/core/types/despesa-configuracao-combustivel.types';
 import { DespesaPeriodoTecnico } from 'app/core/types/despesa-periodo.types';
-import { CustomSnackbarService } from 'app/core/services/custom-snackbar.service';
+import { UsuarioSessao } from 'app/core/types/usuario.types';
+import { UserService } from 'app/core/user/user.service';
+import { Subject } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 
 @Component({
@@ -28,30 +28,23 @@ export class AuditoriaUtilizacaoComponent implements OnInit {
   despesasPeriodoTecnico: DespesaPeriodoTecnico[] = [];
   configuracaoCombustivel: DespesaConfiguracaoCombustivel;
   protected _onDestroy = new Subject<void>();
-  @ViewChild('obsInputControl') obsInputControl: ElementRef;
 
   constructor(
     private _formBuilder: FormBuilder,
     private _route: ActivatedRoute,
     private _userService: UserService,
-    private _snack: CustomSnackbarService,
     private _auditoriaService: AuditoriaService,
+    private _snack: CustomSnackbarService,
   ) {
     this.userSession = JSON.parse(this._userService.userSession);
   }
 
   async ngOnInit() {
     this.inicializarForm();
-
     this.codAuditoria = +this._route.snapshot.paramMap.get('codAuditoria');
     this.codAuditoriaVeiculo = +this._route.snapshot.paramMap.get('codAuditoriaVeiculo');
     this.obterAuditoria();
-
     this.registrarEmitters();
-
-    this.preencherForm();
-
-    
   }
 
   obterAuditoria() {
@@ -90,32 +83,20 @@ export class AuditoriaUtilizacaoComponent implements OnInit {
     });
   }
 
-  private preencherForm(): void {
-    this.form.controls['valorTanque'].setValue(this.auditoria?.valorTanque);
-    this.form.controls['observacoes'].setValue(this.auditoria?.observacoes);
-    this.form.controls['dataHoraRetiradaVeiculo'].setValue(this.auditoria?.dataHoraRetiradaVeiculo);
-    this.form.controls['totalDiasEmUso'].setValue(this.auditoria?.totalDiasEmUso);
-  
-  }
-
   private registrarEmitters() { 
-    fromEvent(this.obsInputControl.nativeElement, 'keyup').pipe(
-      map((event: any) => {
-        return event.target.value;
-      })
-      , debounceTime(1000)
-      , distinctUntilChanged()).subscribe((text: string) => {
-        this.auditoria.observacoes = text;
-        this._auditoriaService.atualizar(this.auditoria).subscribe(() => {
-          this._snack.exibirToast('Descrição da observação atualizada', 'success');
-        },
-        (error) => {
-          this._snack.exibirToast('Erro ao atualizar a descrição da observação', 'danger');
-        });
-      });
+    this.form.controls['observacoes']
+			.valueChanges
+			.subscribe((texto) => {
+        console.log(texto);
 
-      console.log(this.auditoria.observacoes);
-      
+        //this.auditoria.observacoes = texto;
+        
+        console.log(this.auditoria.observacoes);
+        
+        
+        // this._auditoriaService.atualizar(this.auditoria).subscribe();
+        // this._snack.exibirToast('Observação atualizada', 'success');
+			});
    }
 
   validarCorKMParticular(auditoria) {

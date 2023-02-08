b8919804903e17696e80870a3424fff7547570d8
robsonklima <robsonklima@gmail.com>
2023-02-08 14:38:10 -0300

    REF

--- SAT.FRONT/src/app/modules/main/laboratorio/laboratorio-dashboard/laboratorio-dashboard-itens-antigos/laboratorio-dashboard-itens-antigos.component.ts
+++ SAT.FRONT/src/app/modules/main/laboratorio/laboratorio-dashboard/laboratorio-dashboard-itens-antigos/laboratorio-dashboard-itens-antigos.component.ts
@@ -1,14 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexXAxis,
+  ApexPlotOptions
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+};
 
 @Component({
   selector: 'app-laboratorio-dashboard-itens-antigos',
   templateUrl: './laboratorio-dashboard-itens-antigos.component.html'
 })
 export class LaboratorioDashboardItensAntigosComponent implements OnInit {
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+  isLoading: boolean = true;
 
-  constructor() { }
+  constructor(
+    private _cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    this.montarGrafico();
   }
 
+  montarGrafico() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "basic",
+          data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: [
+          "South Korea",
+          "Canada",
+          "United Kingdom",
+          "Netherlands",
+          "Italy",
+          "France",
+          "Japan",
+          "United States",
+          "China",
+          "Germany"
+        ]
+      }
+    };
+
+    this._cdr.detectChanges();
+    this.isLoading = false;
+  }
 }

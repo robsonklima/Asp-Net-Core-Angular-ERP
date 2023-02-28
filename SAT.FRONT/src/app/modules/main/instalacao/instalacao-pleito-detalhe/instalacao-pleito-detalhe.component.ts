512d7fd0639cad74013ceb041b50b407c1de1ad0
robsonklima <robsonklima@gmail.com>
2023-02-28 10:11:12 -0300

    Tela de edicao de pleito (front)

--- SAT.FRONT/src/app/modules/main/instalacao/instalacao-pleito-detalhe/instalacao-pleito-detalhe.component.ts
+++ SAT.FRONT/src/app/modules/main/instalacao/instalacao-pleito-detalhe/instalacao-pleito-detalhe.component.ts
@@ -1,14 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'app/core/user/user.service';
+import { UserSession } from 'app/core/user/user.types';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-instalacao-pleito-detalhe',
   templateUrl: './instalacao-pleito-detalhe.component.html'
 })
 export class InstalacaoPleitoDetalheComponent implements OnInit {
+  codInstalPleito: number;
+  isAddMode: boolean;
+  isLoading: boolean = true;
+  userSession: UserSession;
+  protected _onDestroy = new Subject<void>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _userService: UserService,
+    private _route: ActivatedRoute,
+  ) {
+    this.userSession = JSON.parse(this._userService.userSession);
   }
 
+  ngOnInit(): void {
+    this.isLoading = true;
+		this.codInstalPleito = +this._route.snapshot.paramMap.get('codInstalPleito');
+		this.isAddMode = !this.codInstalPleito;
+		this.inicializarForm();
+  }
+
+  inicializarForm() {
+    
+  }
+
+  exportar() {
+    
+  }
+
+  ngOnDestroy() {
+		this._onDestroy.next();
+		this._onDestroy.complete();
+	}
 }

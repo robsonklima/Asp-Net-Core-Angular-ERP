fa45f5be2a52d2a14b426ae7024e932b80baacad
robsonklima <robsonklima@gmail.com>
2023-04-18 14:32:31 -0300

    Correcao layout login

--- SAT.FRONT/tailwind.config.js
+++ SAT.FRONT/tailwind.config.js
@@ -126,7 +126,7 @@ const config = {
         },
         extend  : {
             backgroundImage: theme => ({
-                'fabrica': "url('assets/images/backgrounds/fabrica-2.jpg')",
+                'fabrica': "url('assets/images/backgrounds/fabrica.png')",
             }),
             animation : {
                 'spin-slow': 'spin 3s linear infinite'

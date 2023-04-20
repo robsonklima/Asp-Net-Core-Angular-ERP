ea24e5d4b5f4d36562e9ed4f5f842a2927648291
robsonklima <robsonklima@gmail.com>
2023-04-20 16:26:26 -0300

    Correcao Exportacao Técnicos

--- SAT.SERVICES/Services/ExportacaoService.Tecnico.cs
+++ SAT.SERVICES/Services/ExportacaoService.Tecnico.cs
@@ -11,17 +11,17 @@ namespace SAT.SERVICES.Services {
             var sheet = tecnicos.Select(tecnico =>
                             new
                             {
-                                Codigo = tecnico.CodTecnico,
+                                Codigo = tecnico?.CodTecnico,
                                 Nome = tecnico.Nome ?? Constants.NENHUM_REGISTRO,
                                 Filial = tecnico.Filial?.NomeFilial ?? Constants.NENHUM_REGISTRO,
-                                Autorizada = tecnico.Autorizada?.NomeFantasia ?? Constants.NENHUM_REGISTRO,
-                                Perfil = tecnico.Usuario?.Perfil?.NomePerfil ?? Constants.NENHUM_REGISTRO,
-                                FonePerto = tecnico.FonePerto ?? Constants.NENHUM_REGISTRO,
-                                FoneParticular = tecnico.FoneParticular ?? Constants.NENHUM_REGISTRO,
-                                DataAdmissao = tecnico.DataAdmissao,
-                                Ativo = tecnico.IndAtivo == 1 ? "SIM" : "NÃO",
-                                Cidade = tecnico.Cidade.NomeCidade ?? Constants.NENHUM_REGISTRO,
-                                Regiao = tecnico.Regiao.NomeRegiao ?? Constants.NENHUM_REGISTRO
+                                Autorizada = tecnico?.Autorizada?.NomeFantasia ?? Constants.NENHUM_REGISTRO,
+                                Perfil = tecnico?.Usuario?.Perfil?.NomePerfil ?? Constants.NENHUM_REGISTRO,
+                                FonePerto = tecnico?.FonePerto ?? Constants.NENHUM_REGISTRO,
+                                FoneParticular = tecnico?.FoneParticular ?? Constants.NENHUM_REGISTRO,
+                                DataAdmissao = tecnico?.DataAdmissao,
+                                Ativo = tecnico?.IndAtivo == 1 ? "SIM" : "NÃO",
+                                Cidade = tecnico?.Cidade?.NomeCidade ?? Constants.NENHUM_REGISTRO,
+                                Regiao = tecnico?.Regiao?.NomeRegiao ?? Constants.NENHUM_REGISTRO
                             });
 
             var wsOs = Workbook.Worksheets.Add("Tecnicos");

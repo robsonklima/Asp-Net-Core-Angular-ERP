983726debde75fb59ab1a8dea1bd3af1425201de
robsonklima <robsonklima@gmail.com>
2023-05-31 12:19:03 -0300

    REF

--- SAT.API/Controllers/IntegracaoClienteController.cs
+++ SAT.API/Controllers/IntegracaoClienteController.cs
@@ -1,44 +1,28 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using SAT.MODELS.Entities;
-using SAT.MODELS.Entities.Params;
-using SAT.MODELS.ViewModels;
 using SAT.SERVICES.Interfaces;
 
 namespace SAT.API.Controllers
 {
-    [Authorize]
     [EnableCors("CorsApi")]
     [Route("api/[controller]")]
     [ApiController]
     public class IntegracaoClienteController : ControllerBase
     {
-        private readonly IIntegracaoClienteService _IntegracaoClienteService;
+        private readonly IIntegracaoClienteService _integracaoClienteService;
 
         public IntegracaoClienteController(
-            IIntegracaoClienteService IntegracaoClienteService
+            IIntegracaoClienteService integracaoClienteService
         )
         {
-            _IntegracaoClienteService = IntegracaoClienteService;
+            _integracaoClienteService = integracaoClienteService;
         }
 
         [HttpPost]
         public IntegracaoCliente Post([FromBody] IntegracaoCliente IntegracaoCliente)
         {
-            return _IntegracaoClienteService.Criar(IntegracaoCliente);
-        }
-
-        [HttpPut]
-        public void Put([FromBody] IntegracaoCliente IntegracaoCliente)
-        {
-            _IntegracaoClienteService.Atualizar(IntegracaoCliente);
-        }
-
-        [HttpDelete("{CodInstalTipoPleito}")]
-        public void Delete(int codInstalTipoPleito)
-        {
-            _IntegracaoClienteService.Deletar(codInstalTipoPleito);
+            return _integracaoClienteService.Integrar(IntegracaoCliente);
         }
     }
 }

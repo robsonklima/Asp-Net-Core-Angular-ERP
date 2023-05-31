f5acd04ca056f132d3efe065c75badd69cc64722
robsonklima <robsonklima@gmail.com>
2023-05-30 22:38:39 -0300

    REF

--- SAT.FRONT/src/environments/environment.ts
+++ SAT.FRONT/src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://localhost:5001/api',
-    // clientUrl: 'http://localhost:4200'
-    apiUrl: 'https://sat.perto.com.br/SAT.V2.API/api',
-    clientUrl: 'https://sat.perto.com.br/SAT.V2.FRONTEND'
+    apiUrl: 'https://localhost:5001/api',
+    clientUrl: 'http://localhost:4200'
 };

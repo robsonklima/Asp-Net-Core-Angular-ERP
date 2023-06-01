25d5d6f9613c6bd31adb201456d95ad531473bbd
robsonklima <robsonklima@gmail.com>
2023-06-01 10:40:59 -0300

    REF

--- SAT.MODELS/Entities/Params/IntegracaoClienteParameters.cs
+++ SAT.MODELS/Entities/Params/IntegracaoClienteParameters.cs
@@ -2,7 +2,7 @@ using SAT.MODELS.Entities.Helpers;
 
 namespace SAT.MODELS.Entities.Params
 {
-    public class IntegracaoClienteParameters: QueryStringParameters
+    public class IntegracaoClienteParameters
     {
         public string NumOscliente { get; set; }
         public string Chave { get; set; }

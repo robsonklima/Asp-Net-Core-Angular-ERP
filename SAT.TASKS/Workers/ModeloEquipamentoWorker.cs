d3892158dce339a3ebc49d716715335f8e1f2d3b
robsonklima <robsonklima@gmail.com>
2023-06-01 11:49:53 -0300

    REF

--- SAT.TASKS/Workers/ModeloEquipamentoWorker.cs
+++ SAT.TASKS/Workers/ModeloEquipamentoWorker.cs
@@ -1,15 +1,28 @@
 using SAT.SERVICES.Interfaces;
+using SAT.SERVICES.Services;
+using SAT.MODELS.Entities.Params;
 
 namespace SAT.TASKS;
 public partial class ModeloEquipamentoWorker : BackgroundService
 {
+    private readonly IContratoEquipamentoService _contratoEquipamentoService;
+    public ModeloEquipamentoWorker (
+        IContratoEquipamentoService contratoEquipamentoService)
+    {
+        _contratoEquipamentoService = contratoEquipamentoService;
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
-             
+                var parametros = new ContratoEquipamentoParameters {};
+                var contratos = _contratoEquipamentoService.ObterPorParametros(parametros).Items;
+
+                // Laco de repeticao para cada contrato equipamento
+                // verificando se possui parque ativo. Inativar/Ativar conforme o gosto
             }
             catch (Exception)
             {

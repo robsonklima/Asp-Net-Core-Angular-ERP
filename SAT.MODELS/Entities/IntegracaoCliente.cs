a3cb53acefaf614a984ac54ae328d0fc3f0852d3
robsonklima <robsonklima@gmail.com>
2023-06-01 16:35:05 -0300

    REF

--- SAT.MODELS/Entities/IntegracaoCliente.cs
+++ SAT.MODELS/Entities/IntegracaoCliente.cs
@@ -9,6 +9,7 @@ namespace SAT.MODELS.Entities
         public string NumIncidentePerto { get; set; }
         public string RelatoCliente { get; set; }
         public string NumSerie { get; set; }
+        public string NumAgencia { get; set; }
         [JsonIgnore]
         public EquipamentoCliente Equipamento { get; set; }
     }

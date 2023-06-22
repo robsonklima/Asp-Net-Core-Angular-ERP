05f20a891c15876813e365c8573cb8b4cffd9c6a
robsonklima <robsonklima@gmail.com>
2023-06-22 13:03:04 -0300

    REF

--- SAT.MODELS/Enums/SatTipoEnum.cs
+++
@@ -1,8 +0,0 @@
-namespace SAT.MODELS.Enums
-{
-    public enum SatTaskTipoEnum
-    {
-        public const string TASK_INTEGRACAO_BANRISUL = "INT_BANRISUL";
-        public const string TASK_INTEGRACAO_BB = "INT_BB";
-    }
-}
--- SAT.TASKS/Startup.cs
+++ SAT.TASKS/Startup.cs
@@ -1,58 +1,36 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using SAT.INFRA.Context;
-using Quartz;
-using Quartz.Impl;
-using Quartz.Spi;
+using Microsoft.EntityFrameworkCore;
+using Autofac;
+using SAT.IOC;
+using Autofac.Extensions.DependencyInjection;
+using SAT.TASKS;
+using Microsoft.AspNetCore.Builder;
 using SAT.MODELS.Entities.Constants;
 
-namespace SAT.TASKS
-{
-    public class Startup
+using IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
     {
-        public Startup(IConfiguration configuration)
+        options.ServiceName = "SAT.TASKS";
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<AppDbContext>(
+                options => options.UseSqlServer(context.Configuration.GetConnectionString(Constants.DB_PROD),
+                sqlServerOptions => sqlServerOptions.CommandTimeout(180)));
+
+        services.AddHttpContextAccessor();
+        services.AddMvc();
+        services.AddSession();
+
+        services.Configure<IISOptions>(o =>
         {
-            Configuration = configuration;
-        }
+            o.ForwardClientCertificate = false;
+        });
 
-        public IConfiguration Configuration { get; }
+        services.AddHostedService<Worker>();
+    })
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new ModuleIOC()))
+    .Build();
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            string connString = Configuration.GetConnectionString(Constants.DB_PROD);
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connString));
-            
-            services.AddSingleton<IJobFactory, JobFactory>();
-            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
-            services.AddSingleton<QuartzJobRunner>();
-            services.AddHostedService<QuartzHostedService>();
-
-            // Jobs Initializing
-            services.AddScoped<IntegracaoBanrisulJob>();
-            services.AddScoped<ModeloEquipamentoJob>();
-            services.AddScoped<IntegracaoMRPJob>();
-            services.AddScoped<IntegracaoBBJob>();
-
-            // Jogs Schedules
-            services.AddSingleton(new JobSchedule(
-                jobType: typeof(IntegracaoBanrisulJob),
-                cronExpression: Constants.CRON_EVERY_1_MIN));
-        }
-
-        [Obsolete]
-        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("SAT.TASKS");
-            });
-        }
-    }
-}
+await host.RunAsync();

eb3fc9a7ef6a4082b701ab1f69a517475a48a0a7
robsonklima <robsonklima@gmail.com>
2023-06-22 12:58:56 -0300

    REF

---
+++ SAT.MODELS/Enums/SatTipoEnum.cs
@@ -0,0 +1,8 @@
+namespace SAT.MODELS.Enums
+{
+    public enum SatTaskTipoEnum
+    {
+        public const string TASK_INTEGRACAO_BANRISUL = "INT_BANRISUL";
+        public const string TASK_INTEGRACAO_BB = "INT_BB";
+    }
+}

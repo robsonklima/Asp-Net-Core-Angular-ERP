6db872871943af3401ba3d11d3ca5b0739a6d3bb
robsonklima <robsonklima@gmail.com>
2023-06-23 08:58:30 -0300

    REF

--- SAT.SERVICES/Services/IntegracaoBBService.cs
+++ SAT.SERVICES/Services/IntegracaoBBService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using NLog;
 using NLog.Fluent;
 using SAT.MODELS.Entities;
@@ -30,96 +32,102 @@ namespace SAT.SERVICES.Services
 
         public void Processar()
         {
-            _logger.Info()
-                .Message("Iniciado o processamento da {}", Constants.INTEGRACAO_BB)
-                .Property("application", Constants.SISTEMA_CAMADA_TASKS)
-                .Write();
+            _logger.Info($"Iniciando o procesamento: { Constants.INTEGRACAO_BB }");
 
-            // var ordens = ObterChamadosArquivo();
+            List<string> files = LerDiretorio();
 
-            // ordens.ForEach(os => {
-            //     AbreChamado(os);
-            // });
+            _logger.Info($"Encontrados: { files.Count() } para processamento");
+
+            files.ForEach((file) => {
+                var chamados = ExtrairChamadosArquivo(file);
+
+                chamados.ForEach(chamado => {
+                    //var os = _ordemServicoService.Criar(chamado);
+                    //_logger.Info($"Aberto chamado: { os.CodOS }");
+                    CriarArquivoRetorno(chamado);
+                });
+            });
         }
 
-        public List<OrdemServico> ObterChamadosArquivo()
+        public List<OrdemServico> ExtrairChamadosArquivo(string conteudo)
         {
             List<OrdemServico> ordens = new();
 
-            // Ler o arquivo
-            string linha = "120237816000650000MANUTENCAO781613SOP-JD.CAMBURI      08:00:00/18:00:0027   33376555000000000000191R.VICTORINO CARDOSO,235            JARDIM CAMBURI           VITORIA                     ES02909082008PLANTAO DIAS UTEIS, DE 8H AS 18H. SOLUCAO EM 30H  7417221520162TAA-TERMINAL DE AUTOATENDIMENTO    7417221520162-8  0                      036500URGENTE   LEITORA DE CARTíES COM DEFEITO                                                                                                    0001MANUT. CORRETIVA    400306444    PERTO SA PERIFERICOS PARA AUTOMACAO     F681193720062023161010F6811937MARCOS      SAA                                               20062023161010        0000000001202100000467          10000000000000000000005                                                                                                   N";
-
-            OrdemServicoBB osCliente = new OrdemServicoBB {
-               NumOSCliente = linha.Substring(2, 15),
-               TipoOS = linha.Substring(19, 28),
-               NumAgencia = linha.Substring(29, 32)?.PadLeft(5),
-               DCPosto = linha.Substring(33, 34)?.PadLeft(2),
-               NomeAgencia = linha.Substring(35, 54),
-               HoraInicial = linha.Substring(55, 71),
-               HoraFinal = linha.Substring(55, 71),
-               DDDDependencia = linha.Substring(72, 75),
-               Telefone = linha.Substring(76, 84),
-               CGC = linha.Substring(85, 99),
-               Endereco = linha.Substring(100, 134),
-               Bairro = linha.Substring(135, 159),
-               Cidade = linha.Substring(160, 187),
-               UF = linha.Substring(188, 189),
-               CEP = linha.Substring(190, 198),
-               Criticidade = linha.Substring(199, 200),
-               DescCriticidade = linha.Substring(201, 250),
-               NumBem = linha.Substring(251, 263),
-               DescricaoBem = linha.Substring(264, 298),
-               NumSerie = linha.Substring(299, 315),
-               Modelo = linha.Substring(316, 325),
-               BilheteOS = linha.Substring(326, 338),
-               GarantiaBem = linha.Substring(339, 342),
-               Impacto = linha.Substring(343, 344),
-               DescricaoImpacto = linha.Substring(345, 354),
-               Defeito = linha.Substring(355, 484),
-               TipoManutencao = linha.Substring(485, 488),
-               DescricaoManutencao = linha.Substring(489, 508),
-               CodigoFornecedor = linha.Substring(509, 521),
-               DescricaoFornecedor = linha.Substring(522, 561),
-               MatriculaAberturaOS = linha.Substring(562, 569),
-               DataAberturaOS = linha.Substring(570, 577),
-               HoraAberturaOS = linha.Substring(578, 583),
-               NomeContato = linha.Substring(584, 603),
-               NumeroChamada = linha.Substring(644, 653),
-               DataChamadaOS = linha.Substring(654, 661),
-               HoraChamadaOS = linha.Substring(662, 667),
-               ChaveDoChamador = linha.Substring(668, 675),
-               NumeroDiasAtendimento = linha.Substring(676, 679),
-               NumeroHorasAtendimento = linha.Substring(680, 685),
-               NumeroContrato = linha.Substring(686, 697),
-               CodigoMantenedora = linha.Substring(698, 707),
-               IndOSGarantia = linha.Substring(708, 708),
-               DataAgendaOS = linha.Substring(709, 716),
-               HoraAgendaOS = linha.Substring(717, 722),
-               PrefixoDependenciaDestino = linha.Substring(723, 726),
-               SubordinadaDependenciaDestino = linha.Substring(727, 728),
-               CodigoTipoBem = linha.Substring(729, 730),
-               TextoMotivoManutencao = linha.Substring(731, 830)
+            OrdemServicoBB osCliente = new OrdemServicoBB
+            {
+                NumOSCliente = conteudo.Substring(2, 15),
+                TipoOS = conteudo.Substring(19, 28),
+                NumAgencia = conteudo.Substring(29, 32)?.PadLeft(5),
+                DCPosto = conteudo.Substring(33, 34)?.PadLeft(2),
+                NomeAgencia = conteudo.Substring(35, 54),
+                HoraInicial = conteudo.Substring(55, 71),
+                HoraFinal = conteudo.Substring(55, 71),
+                DDDDependencia = conteudo.Substring(72, 75),
+                Telefone = conteudo.Substring(76, 84),
+                CGC = conteudo.Substring(85, 99),
+                Endereco = conteudo.Substring(100, 134),
+                Bairro = conteudo.Substring(135, 159),
+                Cidade = conteudo.Substring(160, 187),
+                UF = conteudo.Substring(188, 189),
+                CEP = conteudo.Substring(190, 198),
+                Criticidade = conteudo.Substring(199, 200),
+                DescCriticidade = conteudo.Substring(201, 250),
+                NumBem = conteudo.Substring(251, 263),
+                DescricaoBem = conteudo.Substring(264, 298),
+                NumSerie = conteudo.Substring(299, 315),
+                Modelo = conteudo.Substring(316, 325),
+                BilheteOS = conteudo.Substring(326, 338),
+                GarantiaBem = conteudo.Substring(339, 342),
+                Impacto = conteudo.Substring(343, 344),
+                DescricaoImpacto = conteudo.Substring(345, 354),
+                Defeito = conteudo.Substring(355, 484),
+                TipoManutencao = conteudo.Substring(485, 488),
+                DescricaoManutencao = conteudo.Substring(489, 508),
+                CodigoFornecedor = conteudo.Substring(509, 521),
+                DescricaoFornecedor = conteudo.Substring(522, 561),
+                MatriculaAberturaOS = conteudo.Substring(562, 569),
+                DataAberturaOS = conteudo.Substring(570, 577),
+                HoraAberturaOS = conteudo.Substring(578, 583),
+                NomeContato = conteudo.Substring(584, 603),
+                NumeroChamada = conteudo.Substring(644, 653),
+                DataChamadaOS = conteudo.Substring(654, 661),
+                HoraChamadaOS = conteudo.Substring(662, 667),
+                ChaveDoChamador = conteudo.Substring(668, 675),
+                NumeroDiasAtendimento = conteudo.Substring(676, 679),
+                NumeroHorasAtendimento = conteudo.Substring(680, 685),
+                NumeroContrato = conteudo.Substring(686, 697),
+                CodigoMantenedora = conteudo.Substring(698, 707),
+                IndOSGarantia = conteudo.Substring(708, 708),
+                DataAgendaOS = conteudo.Substring(709, 716),
+                HoraAgendaOS = conteudo.Substring(717, 722),
+                PrefixoDependenciaDestino = conteudo.Substring(723, 726),
+                SubordinadaDependenciaDestino = conteudo.Substring(727, 728),
+                CodigoTipoBem = conteudo.Substring(729, 730),
+                TextoMotivoManutencao = conteudo.Substring(731, 830)
             };
 
             EquipamentoContrato equipamentoContrato = (EquipamentoContrato)_equipamentoContratoService
-                .ObterPorParametros(new EquipamentoContratoParameters {
+                .ObterPorParametros(new EquipamentoContratoParameters
+                {
                     NumSerie = osCliente.NumSerie,
                     CodClientes = Constants.CLIENTE_BB.ToString()
                 })
                 .Items.FirstOrDefault();
 
-            if (equipamentoContrato == null) 
+            if (equipamentoContrato == null)
                 throw new Exception("Não encontrou equipamento");
 
             LocalAtendimento localAtendimento = (LocalAtendimento)_localAtendimentoService
-                .ObterPorParametros(new LocalAtendimentoParameters {
+                .ObterPorParametros(new LocalAtendimentoParameters
+                {
                     NumAgencia = osCliente.NumAgencia,
                     DCPosto = osCliente.DCPosto,
                     CodCliente = Constants.CLIENTE_BB
                 })
                 .Items.FirstOrDefault();
 
-            bool isOSAberta = _ordemServicoService.ObterPorParametros(new OrdemServicoParameters {
+            bool isOSAberta = _ordemServicoService.ObterPorParametros(new OrdemServicoParameters
+            {
                 NumOSCliente = osCliente.NumOSCliente,
                 CodCliente = Constants.CLIENTE_BB
             }).Items.FirstOrDefault() != null;
@@ -127,21 +135,22 @@ namespace SAT.SERVICES.Services
             if (isOSAberta)
                 throw new Exception("OS ja aberta");
 
-            if (localAtendimento == null) 
+            if (localAtendimento == null)
                 throw new Exception("Não encontrou local");
 
             var observacaoCliente = $@"
-                OS INTEGRADA DIA { DateTime.Now.ToString() }
-                SÉRIE: { equipamentoContrato.NumSerie }
-                AGÊNCIA: { localAtendimento.NumAgencia }/{ localAtendimento.DCPosto } - { localAtendimento.NomeLocal }
-                ENDEREÇO: { osCliente.Endereco }
-                BAIRRO: { osCliente.Bairro }
-                UF: { osCliente.UF }
-                CIDADE: { osCliente.Cidade }
-                CEP: { osCliente.CEP }
+                OS INTEGRADA DIA {DateTime.Now.ToString()}
+                SÉRIE: {equipamentoContrato.NumSerie}
+                AGÊNCIA: {localAtendimento.NumAgencia}/{localAtendimento.DCPosto} - {localAtendimento.NomeLocal}
+                ENDEREÇO: {osCliente.Endereco}
+                BAIRRO: {osCliente.Bairro}
+                UF: {osCliente.UF}
+                CIDADE: {osCliente.Cidade}
+                CEP: {osCliente.CEP}
             ";
 
-            var ordemServico = new OrdemServico {
+            var ordemServico = new OrdemServico
+            {
                 CodCliente = Constants.CLIENTE_BB,
                 CodPosto = (int)localAtendimento.CodPosto,
                 CodEquipContrato = equipamentoContrato.CodEquipContrato,
@@ -168,9 +177,45 @@ namespace SAT.SERVICES.Services
             return ordens;
         }
 
-        public void AbreChamado(OrdemServico os) 
+        public void CriarArquivoRetorno(OrdemServico chamado)
         {
+            string fileName = "CRM549R.xPerto01." + DateTime.Now.ToString("ddMMyyyyHHMMsss");
+            string target = Directory.GetCurrentDirectory() + "/Output";
 
+            using (StreamWriter w = new StreamWriter(target))
+            {
+                w.WriteLine("Teste 1");
+                w.WriteLine("Teste 2");
+                w.WriteLine("Teste 3");
+            }
+        }
+
+        public List<string> LerDiretorio()
+        {
+            string target = Directory.GetCurrentDirectory() + "/Input";
+            DirectoryInfo dirInfo = new DirectoryInfo(target);
+            FileInfo[] files = dirInfo.GetFiles("crm549.*.BB");
+            List<string> linhas = new();
+
+            foreach (FileInfo file in files)
+            {
+                using (StreamReader sr = File.OpenText(target + "/" + file.Name))
+                {
+                    string linha = String.Empty;
+
+                    while ((linha = sr.ReadLine()) is not null)
+                    {
+                        char primeiroCaractere = linha[0];
+
+                        if (primeiroCaractere == '1')
+                        {
+                            linhas.Add(linha);
+                        }
+                    }
+                }
+            }
+
+            return linhas;
         }
     }
 }

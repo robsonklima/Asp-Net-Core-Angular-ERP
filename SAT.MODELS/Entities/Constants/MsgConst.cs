ae78b14df6aea25b6730a1cf838aecf8a81d9dbe
robsonklima <robsonklima@gmail.com>
2023-06-26 22:19:55 -0300

    REF

--- SAT.MODELS/Entities/Constants/MsgConst.cs
+++ SAT.MODELS/Entities/Constants/MsgConst.cs
@@ -4,17 +4,11 @@ namespace SAT.MODELS.Entities.Constants
     {
         public static readonly string INI_PROC = "Iniciando o processamento";
         public static readonly string FIN_PROC = "Finalizando o processamento";
-        public static readonly string OBTENDO_PROCESSOS_CHAMADO = "Obtendo os processos do chamado";
         public static readonly string INI_PROC_FILA = "Iniciando o processamento da fila";
         public static readonly string FIN_PROC_FILA = "Finalizando o processamento da fila";
-        public static readonly string PROCESSO_PENDENTE = "Processo pendente";
-        public static readonly string TASK_NAO_POSSUI_PROCESSOS = "A task nao possui processos";
         public static readonly string INI_PROC_TASKS = "Iniciando o processamento das tasks";
-        public static readonly string FIN_PROC_TASKS = "Finalizando o processamento das tasks";
         public static readonly string LENDO_TIPO = "Lendo arquivos do tipo";
         public static readonly string EXISTE_TASK_PENDENTE = "Existe uma task pendente e sera processada";
-        public static readonly string NAO_POSSUI_PERMISSAO_PROCESSAR = "A task nao possui permissao para processar";
-        public static readonly string QTD_CHAMADOS_ENVIO = "Chamados obtidos para envio";
         public static readonly string LENDO_LINHA = "Lendo a linha";
         public static readonly string MENOR_ESPECIFICADO = "Conteudo do arquivo menor que o especificado";
         public static readonly string ENCONTRADOS = "Arquivos encontrados arquivos para processamento";
@@ -39,34 +33,11 @@ namespace SAT.MODELS.Entities.Constants
         public static readonly string INI_ARQ_RET = "Iniciando a criacao dos arquivos de retorno";
         public static readonly string FIN_ARQ_RET = "Finalizando a criacao dos arquivos de retorno";
         public static readonly string OCORREU_ERRO = "Ocorreu um erro";
-        public static readonly string REG_NAO_ENCONTRADO = "Registro nao encontrado";
-        public static readonly string NENHUM_REG_ENCONTRADO = "Nenhum registro encontrado";
-        public static readonly string ULT_TASK_OBTIDA = "Ultima task obtida";
-        public static readonly string OBTENDO_ULT_TASK = "Obtendo a ultima task do tipo";
-        public static readonly string INIC_PROCESSO = "Iniciando";
         public static readonly string FIN_PROCESSO = "Iniciando";
-        public static readonly string OBTENDO_TASKS = "Obtendo tasks";
-        public static readonly string OBTENDO_TASKS_PENDENTES = "Obtendo tasks pendentes";
-        public static readonly string TASKS_OBTIDAS = "Tasks obtidas";
         public static readonly string ERR_API_CLIENTE = "Nao foi obtido retorno da API do cliente";
-        public static readonly string OBTENDO_PROCESSOS = "Obtendo processos";
-        public static readonly string OBTENDO_PROCESSOS_PENDENTES = "Obtendo processos pendentes";
-        public static readonly string QTD_PROCESSOS = "Processos encontrados";
-        public static readonly string PROC_PROCESSOS = "Processando processos";
-        public static readonly string OS_PERTO = "Ordem servico Perto";
         public static readonly string INIC_TRANSMISSAO = "Iniciando a transmissao";
         public static readonly string FIN_TRANSMISSAO = "Finalizando a transmissao";
-        public static readonly string CRIANDO_PROCESSO = "Criando o processo";
-        public static readonly string PROCESSO_CRIADO = "Processo criado com sucesso";
         public static readonly string CRIANDO_TASK = "Criando a task";
         public static readonly string TASK_CRIADA = "Task criada com sucesso";
-        public static readonly string ATUALIZANDO_PROCESSO = "Atualizando o processo";
-        public static readonly string ATUALIZANDO_TASK = "Atualizando a task";
-        public static readonly string TASK_ATUALIZADA = "Task atualizada com sucesso";
-        public static readonly string PROCESSO_ATUALIZADO = "Processo atualizado com sucesso";
-        public static readonly string OBTENDO_TIPOS = "Obtendo tipos de tasks";
-        public static readonly string TIPOS_OBTIDOS = "Obtidos tipos de tasks";
-        public static readonly string OBTENDO_PERMISSAO = "Obtendo permissao para processar";
-        public static readonly string TASK_PENDENTE = "Task pendente processamento";
     }
 }

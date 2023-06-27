9decfe98066fd3314d29f6717c008884cf905974
robsonklima <robsonklima@gmail.com>
2023-06-27 11:52:31 -0300

    REF

--- SAT.SERVICES/Services/IntegracaoBBService.cs
+++ SAT.SERVICES/Services/IntegracaoBBService.cs
@@ -81,7 +81,9 @@ namespace SAT.SERVICES.Services
         {
             try
             {
-                _logger.Info(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+                string curr = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+                _logger.Info(curr);
 
                 List<string> retorno = new();
                 DirectoryInfo dirInfo = new DirectoryInfo(Constants.DIR_INTEGRACAO_BB_INPUT);

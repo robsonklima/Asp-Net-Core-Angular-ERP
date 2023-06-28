56bd9ce3ee7cc6276aa12750615271d3d6266be7
robsonklima <robsonklima@gmail.com>
2023-06-27 21:11:21 -0300

    REF

--- SAT.TASKS/DedicatedWorkers/Worker.Tasks.Zaffari.cs
+++ SAT.TASKS/DedicatedWorkers/Worker.Tasks.Zaffari.cs
@@ -7,13 +7,10 @@ namespace SAT.TASKS
     {
         private async Task<SatTask> IntegrarZaffariAsync(SatTask task, IEnumerable<OrdemServico> chamados)
         {
-            var chamadosIntegracao = chamados
+            var chamadosZaffari = chamados
                 .Where(os => os.CodCliente == Constants.CLIENTE_ZAFFARI)
                 .Where(os => os.IndIntegracao == 1)
                 .Where(os => os.IndServico == 1);
-
-            var chamadosZaffari = chamadosIntegracao
-                .Where(os => os.CodCliente == Constants.CLIENTE_ZAFFARI);
 
             await _integracaoZaffariService.ExecutarAsync(chamadosZaffari);
             

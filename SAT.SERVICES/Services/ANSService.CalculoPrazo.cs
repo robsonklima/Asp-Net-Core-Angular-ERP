4bf180bc3222d061c4bbbe79f2247be0ca2ae382
robsonklima <robsonklima@gmail.com>
2023-06-29 19:38:03 -0300

    REF

---
+++ SAT.SERVICES/Services/ANSService.CalculoPrazo.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SAT.MODELS.Entities;
+using SAT.MODELS.Entities.Constants;
+using SAT.MODELS.Entities.Params;
+using SAT.SERVICES.Interfaces;
+using SAT.UTILS;
+
+namespace SAT.SERVICES.Services
+{
+    public partial class ANSService : IANSService
+    {
+        public DateTime? CalcularPrazo(OrdemServico chamado)
+        {
+            var ans = chamado?.EquipamentoContrato?.ANS;
+            DateTime previsao = chamado.DataHoraAberturaOS.Value;
+            if (ans is null)
+                return null;
+
+            var agendamento = chamado.Agendamentos
+                .OrderBy(a => a.DataAgendamento)
+                .FirstOrDefault();
+
+            if (ans.PermiteAgendamento == Constants.SIM && agendamento is not null)
+                previsao = agendamento.DataAgendamento.Value;
+
+            IEnumerable<SATFeriado> feriados = (IEnumerable<SATFeriado>)_feriadoService
+                .ObterPorParametros(new SATFeriadoParameters
+                {
+                    Mes = chamado.DataHoraAberturaOS.Value.Month
+                })
+                .Items;
+
+            for (int i = 0; i < ans.TempoHoras;)
+            {
+                previsao = AplicarHorariosNaoComercais(previsao, ans);
+                previsao = AplicarHorariosComerciais(previsao, ans);
+                previsao = AplicarFeriados(feriados, chamado, ans, previsao);
+                previsao = previsao.AddHours(1);
+                i++;
+            }
+
+            if (ans.ArredondaHoraFinal == Constants.SIM)
+                previsao = previsao.Date + new TimeSpan(ans.HoraFim.Hours, ans.HoraFim.Minutes, ans.HoraFim.Seconds);
+
+            return previsao;
+        }
+
+        private DateTime AplicarHorariosNaoComercais(DateTime previsao, ANS ans)
+        {
+            if (previsao.DayOfWeek == DayOfWeek.Saturday && ans.Sabado == Constants.NAO)
+                return AdicionarDia(previsao, ans);
+
+            if (previsao.DayOfWeek == DayOfWeek.Sunday && ans.Domingo == Constants.NAO)
+                return AdicionarDia(previsao, ans);
+
+            return previsao;
+        }
+
+        private DateTime AplicarHorariosComerciais(DateTime previsao, ANS ans)
+        {
+            if (previsao.TimeOfDay < ans.HoraInicio)
+                return AdicionarDia(previsao, ans);
+
+            if (previsao.TimeOfDay > ans.HoraFim)
+                return AdicionarDia(previsao, ans);
+
+            return previsao;
+        }
+
+        private DateTime AplicarFeriados(IEnumerable<SATFeriado> feriados, OrdemServico chamado, ANS ans, DateTime previsao)
+        {
+            foreach (var f in feriados.Where(f => f.Data.Date == previsao.Date))
+            {
+                string uf = chamado.LocalAtendimento.Cidade.UnidadeFederativa.SiglaUF;
+                string cidade = StringHelper.RemoverAcentos(chamado.LocalAtendimento.Cidade.NomeCidade);
+
+                if (f.Tipo == FeriadoTipoConst.NACIONAL && f.Data == previsao.Date && ans.Feriado == Constants.NAO)
+                    return AdicionarDia(previsao, ans);
+                
+                if (f.Tipo == FeriadoTipoConst.ESTADUAL && f.UF == uf && f.Data == previsao.Date && ans.Feriado == Constants.NAO)
+                    return AdicionarDia(previsao, ans);
+                
+                if (f.Tipo == FeriadoTipoConst.FACULTATIVO && f.Data == previsao.Date && ans.Feriado == Constants.NAO)
+                    return AdicionarDia(previsao, ans);
+                
+                if (f.Tipo == FeriadoTipoConst.MUNICIPAL && f.Municipio == cidade && f.Data == previsao.Date && ans.Feriado == Constants.NAO)
+                    return AdicionarDia(previsao, ans);
+            }
+
+            return previsao;
+        }
+
+        private DateTime AdicionarDia(DateTime previsao, ANS ans)
+        {
+            var hrInicioPrevisao = new TimeSpan(previsao.TimeOfDay.Hours, previsao.TimeOfDay.Minutes, previsao.TimeOfDay.Seconds);
+            var hrInicioANS = new TimeSpan(ans.HoraInicio.Hours, ans.HoraInicio.Minutes, ans.HoraInicio.Seconds);
+            var hrFimANS = new TimeSpan(ans.HoraFim.Hours, ans.HoraFim.Minutes, ans.HoraFim.Seconds);
+            var saldo = previsao.TimeOfDay - hrFimANS;
+            var novaData = (previsao.AddDays(1).Date + hrInicioANS);
+            var novoPrevisao = novaData.AddMilliseconds(saldo.Milliseconds);
+
+            return novoPrevisao;
+        }
+    }
+}
--- SAT.SERVICES/Services/ANSService.cs
+++ SAT.SERVICES/Services/ANSService.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using NLog;
+﻿using NLog;
 using SAT.INFRA.Interfaces;
 using SAT.MODELS.Entities;
-using SAT.MODELS.Entities.Constants;
 using SAT.MODELS.Entities.Params;
 using SAT.MODELS.ViewModels;
 using SAT.SERVICES.Interfaces;
-using SAT.UTILS;
 
 namespace SAT.SERVICES.Services
 {
@@ -63,122 +58,6 @@ namespace SAT.SERVICES.Services
             };
 
             return lista;
-        }
-
-        public DateTime CalcularPrazo(OrdemServico chamado)
-        {
-            var ans = chamado?.EquipamentoContrato?.ANS;
-            DateTime inicio = chamado.DataHoraAberturaOS.Value;
-            DateTime prazo = inicio;
-
-            if (ans is null)
-            {
-                _logger.Error($"{ MsgConst.ANS_NAO_LOCALIZADA }: { chamado.CodOS }");
-
-                return default(DateTime);
-            }
-
-            var agendamento = chamado.Agendamentos
-                .OrderBy(a => a.DataAgendamento)
-                .FirstOrDefault();
-
-            if (ans.PermiteAgendamento == Constants.SIM && agendamento is not null)
-            {
-                inicio = agendamento.DataAgendamento.Value;
-                prazo = inicio;
-            }
-
-            IEnumerable<SATFeriado> feriados = (IEnumerable<SATFeriado>)_feriadoService
-                .ObterPorParametros(new SATFeriadoParameters
-                {
-                    Mes = chamado.DataHoraAberturaOS.Value.Month
-                })
-                .Items;
-
-            for (int i = 0; i < ans.TempoHoras;)
-            {
-                if (prazo.DayOfWeek == DayOfWeek.Saturday && ans.Sabado == Constants.NAO)
-                {
-                    prazo = AdicionarDia(prazo, ans);
-
-                    continue;
-                }
-
-                if (prazo.DayOfWeek == DayOfWeek.Sunday && ans.Domingo == Constants.NAO)
-                {
-                    prazo = AdicionarDia(prazo, ans);
-
-                    continue;
-                }
-
-                if (prazo.TimeOfDay < ans.HoraInicio)
-                {
-                    prazo = AdicionarDia(prazo, ans);
-
-                    continue;
-                }
-
-                if (prazo.TimeOfDay > ans.HoraFim)
-                {
-                    prazo = AdicionarDia(prazo, ans);
-
-                    continue;
-                }
-
-                foreach (var f in feriados.Where(f => f.Data.Date == prazo.Date)) 
-                {
-                    string uf = chamado.LocalAtendimento.Cidade.UnidadeFederativa.SiglaUF;
-                    string cidade = StringHelper.RemoverAcentos(chamado.LocalAtendimento.Cidade.NomeCidade);
-                    
-                    if (f.Tipo == FeriadoTipoConst.NACIONAL && f.Data == inicio.Date && ans.Feriado == Constants.NAO)
-                    {
-                        prazo = AdicionarDia(prazo, ans);
-
-                        continue;
-                    }
-
-                    if (f.Tipo == FeriadoTipoConst.ESTADUAL && f.UF == uf && f.Data == inicio.Date && ans.Feriado == Constants.NAO)
-                    {
-                        prazo = AdicionarDia(prazo, ans);
-
-                        continue;
-                    }
-
-                    if (f.Tipo == FeriadoTipoConst.FACULTATIVO && f.Data == inicio.Date && ans.Feriado == Constants.NAO)
-                    {
-                        prazo = AdicionarDia(prazo, ans);
-
-                        continue;
-                    }
-
-                    if (f.Tipo == FeriadoTipoConst.MUNICIPAL && f.Municipio == cidade && f.Data == inicio.Date && ans.Feriado == Constants.NAO)
-                    {
-                        prazo = AdicionarDia(prazo, ans);
-
-                        continue;
-                    }
-                }
-
-                prazo = prazo.AddHours(1);
-                i++;
-            }
-
-            if (ans.ArredondaHoraFinal == Constants.SIM)
-                prazo = prazo.Date + new TimeSpan(ans.HoraFim.Hours, ans.HoraFim.Minutes, ans.HoraFim.Seconds);
-            
-            return prazo;
-        }
-
-        private DateTime AdicionarDia(DateTime prazo, ANS ans) 
-        {
-            var hrInicioPrazo = new TimeSpan(prazo.TimeOfDay.Hours, prazo.TimeOfDay.Minutes, prazo.TimeOfDay.Seconds);
-            var hrInicioANS = new TimeSpan(ans.HoraInicio.Hours, ans.HoraInicio.Minutes, ans.HoraInicio.Seconds);
-            var hrFimANS = new TimeSpan(ans.HoraFim.Hours, ans.HoraFim.Minutes, ans.HoraFim.Seconds);
-            var saldo = prazo.TimeOfDay - hrFimANS;
-            var novaData = (prazo.AddDays(1).Date + hrInicioANS);
-            var novoPrazo = novaData.AddMilliseconds(saldo.Milliseconds);
-            
-            return novoPrazo;
         }
     }
 }

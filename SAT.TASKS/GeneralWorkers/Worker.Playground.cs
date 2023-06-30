3088626281d10060780aacd942a8e7cb4623d6ad
robsonklima <robsonklima@gmail.com>
2023-06-29 22:12:24 -0300

    REF

--- SAT.TASKS/GeneralWorkers/Worker.Playground.cs
+++ SAT.TASKS/GeneralWorkers/Worker.Playground.cs
@@ -1,5 +1,3 @@
-using SAT.MODELS.Entities;
-
 namespace SAT.TASKS
 {
     public partial class Worker : BackgroundService
@@ -9,16 +7,6 @@ namespace SAT.TASKS
             var chamado = _osService.ObterPorCodigo(8036742);
 
             var prazo = _ansService.CalcularPrazo(chamado);
-
-            var osPrazo = new OSPrazoAtendimento {
-                DataHoraLimiteAtendimento = prazo,
-                CodOS = chamado.CodOS,
-                DataHoraCad = DateTime.Now
-            };
-
-            osPrazo = (OSPrazoAtendimento)_prazoService.Criar(osPrazo);
-
-            _logger.Info($"Calculado ANS para a OS: { osPrazo.CodOS }, id.: { osPrazo.CodOSPrazoAtendimento }");
         }
     }
 }

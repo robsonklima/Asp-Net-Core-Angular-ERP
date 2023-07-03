e73dc4bc92e69726b6e16a056ead9c7dbaca1ceb
robsonklima <robsonklima@gmail.com>
2023-07-03 16:36:12 -0300

    REf

--- SAT.TASKS/GeneralWorkers/Worker.Playground.cs
+++ SAT.TASKS/GeneralWorkers/Worker.Playground.cs
@@ -1,7 +1,11 @@
+using System.Diagnostics;
+
 namespace SAT.TASKS
 {
     public partial class Worker : BackgroundService
     {
+        [Conditional("DEBUG")]
+
         private void IniciarPlaygroundAsync()
         {
             IntegrarBB(null);

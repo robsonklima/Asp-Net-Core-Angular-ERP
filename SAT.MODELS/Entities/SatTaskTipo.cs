e61ec712afa78fcea6d2e6437c4f5b1c29d04de6
ealmanca <ealmanca@gmail.com>
2023-07-04 17:09:22 -0300

    ajuste dias Tasks

--- SAT.MODELS/Entities/SatTaskTipo.cs
+++ SAT.MODELS/Entities/SatTaskTipo.cs
@@ -11,5 +11,12 @@ namespace SAT.MODELS.Entities
         public int TempoRepeticaoMinutos { get; set; }
         public TimeSpan Inicio { get; set; }
         public TimeSpan Fim { get; set; }
+        public byte? IndDomingo { get; set; }
+        public byte? IndSegunda { get; set; }
+        public byte? IndTerca { get; set; }
+        public byte? IndQuarta { get; set; }
+        public byte? IndQuinta { get; set; }
+        public byte? IndSexta { get; set; }
+        public byte? IndSabado { get; set; }
     }
 }

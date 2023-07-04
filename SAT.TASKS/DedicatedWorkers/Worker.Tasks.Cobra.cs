1dc85602d473ab46bb7e7863a654c7d562edd13f
robsonklima <robsonklima@gmail.com>
2023-07-04 13:26:22 -0300

    REF

---
+++ SAT.TASKS/DedicatedWorkers/Worker.Tasks.Cobra.cs
@@ -0,0 +1,12 @@
+using SAT.MODELS.Entities;
+
+namespace SAT.TASKS
+{
+    public partial class Worker : BackgroundService
+    {
+        private void IntegrarCobra(SatTask task)
+        {
+            
+        }
+    }
+}

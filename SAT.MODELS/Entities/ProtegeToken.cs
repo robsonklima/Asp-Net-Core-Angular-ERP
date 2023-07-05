7b835c5b8b212679049570552e873da6f1fc9759
robsonklima <robsonklima@gmail.com>
2023-07-05 10:01:47 -0300

    REF

---
+++ SAT.MODELS/Entities/ProtegeToken.cs
@@ -0,0 +1,10 @@
+namespace SAT.MODELS.Entities
+{
+    public class ProtegeToken
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public string refresh_token { get; set; }
+    }
+}
--- SAT.TASKS/DedicatedWorkers/Worker.Tasks.Protege.cs
+++ SAT.TASKS/DedicatedWorkers/Worker.Tasks.Protege.cs
@@ -8,7 +8,7 @@ namespace SAT.TASKS
 {
     public partial class Worker : BackgroundService
     {
-        private async void ExecutarProtegeAsync(SatTask task, List<OrdemServico> chamados)
+        private async void ExecutarProtegeAsync(SatTask task, IEnumerable<OrdemServico> chamados)
         {
             await Login();
         }
@@ -22,22 +22,20 @@ namespace SAT.TASKS
             {
                 var client = new HttpClient();
                 
-                client.BaseAddress = new Uri(Constants.INTEGRACAO_PROTEGE_API_URL);
+                client.BaseAddress = new Uri(Constants.INTEGRACAO_PROTEGE_API_URL + "Token");
                 client.DefaultRequestHeaders.Add("username", Constants.INTEGRACAO_PROTEGE_USER);
                 client.DefaultRequestHeaders.Add("password", Constants.INTEGRACAO_PROTEGE_PASSWORD);
                 client.DefaultRequestHeaders.Add("client_id", Constants.INTEGRACAO_PROTEGE_CLIENT_ID);
                 client.DefaultRequestHeaders.Add("grant_type", Constants.INTEGRACAO_PROTEGE_GRANT_TYPE);
                 client.DefaultRequestHeaders.Add("auth_mode", Constants.INTEGRACAO_PROTEGE_AUTH_MODE);
 
-                StringContent content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, Constants.APPLICATION_JSON);
-
-                using (var response = await client.PostAsync(Constants.INTEGRACAO_PROTEGE_API_URL, content))
+                using (var response = await client.GetAsync(Constants.INTEGRACAO_PROTEGE_API_URL))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
 
                     if (response.StatusCode == HttpStatusCode.OK)
                     {
-                        var a = JsonConvert.DeserializeObject<dynamic>(apiResponse);
+                        var a = JsonConvert.DeserializeObject<ProtegeToken>(apiResponse);
                     }
                     else
                     {

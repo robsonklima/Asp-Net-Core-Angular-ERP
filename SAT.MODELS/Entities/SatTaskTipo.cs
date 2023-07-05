77dfbf58d52ee99a2278ab3dafdbb97ef3b73c84
robsonklima <robsonklima@gmail.com>
2023-07-05 08:53:18 -0300

    REF

--- SAT.MODELS/Entities/SatTaskTipo.cs
+++ SAT.MODELS/Entities/SatTaskTipo.cs
@@ -11,12 +11,7 @@ namespace SAT.MODELS.Entities
         public int TempoRepeticaoMinutos { get; set; }
         public TimeSpan Inicio { get; set; }
         public TimeSpan Fim { get; set; }
-        public byte? IndDomingo { get; set; }
-        public byte? IndSegunda { get; set; }
-        public byte? IndTerca { get; set; }
-        public byte? IndQuarta { get; set; }
-        public byte? IndQuinta { get; set; }
-        public byte? IndSexta { get; set; }
-        public byte? IndSabado { get; set; }
+        public byte? DiaSemanaInicio { get; set; }
+        public byte? DiaSemanaFim { get; set; }
     }
 }

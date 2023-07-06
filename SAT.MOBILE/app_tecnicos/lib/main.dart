ad5ba5d806005f28c5f28f738f3f8f52b7bebac8
robsonklima <robsonklima@gmail.com>
2023-07-06 14:17:23 -0300

    REff

--- SAT.MOBILE/app_tecnicos/lib/main.dart
+++ SAT.MOBILE/app_tecnicos/lib/main.dart
@@ -12,18 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: appTitle,
       theme: ThemeData(
-        // Define the default brightness and colors.
         brightness: Brightness.dark,
         primaryColor: Colors.lightBlue[800],
-
-        // Define the default font family.
-        fontFamily: 'Georgia',
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
         textTheme: const TextTheme(
           displayLarge: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
-          titleLarge: TextStyle(fontSize: 36, fontStyle: FontStyle.italic),
+          titleLarge: TextStyle(fontSize: 36),
           bodyMedium: TextStyle(fontSize: 14, fontFamily: 'Hind'),
         ),
       ),

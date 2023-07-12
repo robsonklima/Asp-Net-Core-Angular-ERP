3498b9f4ce2f6d30c32554fda2629aa3de2845dc
robsonklima <robsonklima@gmail.com>
2023-07-12 09:49:57 -0300

    REF

--- SAT.MOBILE/src/screens/HomeScreen.js
+++ SAT.MOBILE/src/screens/HomeScreen.js
@@ -1,49 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import api from '../services/api';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-})
+import React, { useEffect, useState } from "react";
+import api from '../services/api'
 
 export default function HomeScreen({ navigation }) {
   const [chamados, setChamados] = useState([]);
-  const [teste, setTeste] = useState({ value: '', error: '' })
 
   useEffect(() => {
-    (async () => {
-      await api
-        .get('OrdemServico', {
-          params: {
-            codTecnico: 1153,
-            pageSize: 10
-          },
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-          }
-        })
-        .then((response) => {
-          setTeste({ value: 'TESTE 2', error: '' })
-          console.log(teste.value)
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    })();
-  }, [])
+    api
+      .get('OrdemServico', {
+        params: {
+          codTecnico: 1153,
+          pageSize: 10
+        }
+      })
+      .then((response) => {
+        response.data.items.forEach(el => { setChamados(chamados.push(el)) });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }, []);
 
   return (
     <div>
-
+      <h1>OLA</h1>
     </div>
   );
 }

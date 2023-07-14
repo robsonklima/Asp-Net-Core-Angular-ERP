39894fba5063b422faa90a38cca0d7847baf1541
robsonklima <robsonklima@gmail.com>
2023-07-14 09:51:58 -0300

    REF

--- SAT.FRONT/src/app/modules/main/docs/docs.component.ts
+++ SAT.FRONT/src/app/modules/main/docs/docs.component.ts
@@ -1,6 +1,6 @@
-import { 
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy,
-  OnInit, ViewChild, ViewEncapsulation
+import {
+    ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy,
+    OnInit, ViewChild, ViewEncapsulation
 } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { FuseNavigationItem } from '@fuse/components/navigation';
@@ -11,14 +11,13 @@ import { UserService } from 'app/core/user/user.service';
 import { UserSession } from 'app/core/user/user.types';
 
 @Component({
-    selector       : 'app-docs',
-    templateUrl    : './docs.component.html',
-    encapsulation  : ViewEncapsulation.None,
+    selector: 'app-docs',
+    templateUrl: './docs.component.html',
+    encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DocsComponent implements OnInit, OnDestroy
-{
-    @ViewChild('matDrawer', {static: true}) matDrawer: MatDrawer;
+export class DocsComponent implements OnInit, OnDestroy {
+    @ViewChild('matDrawer', { static: true }) matDrawer: MatDrawer;
     drawerMode: 'side' | 'over';
     drawerOpened: boolean;
     menuData: FuseNavigationItem[];
@@ -29,27 +28,23 @@ export class DocsComponent implements OnInit, OnDestroy
         private _changeDetectorRef: ChangeDetectorRef,
         private _fuseMediaWatcherService: FuseMediaWatcherService,
         private _userService: UserService
-    )
-    {
+    ) {
         this.userSession = JSON.parse(this._userService.userSession);
     }
 
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         this.obterMenus();
 
         this._fuseMediaWatcherService.onMediaChange$
             .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe(({matchingAliases}) => {
+            .subscribe(({ matchingAliases }) => {
 
                 // Set the drawerMode and drawerOpened
-                if ( matchingAliases.includes('md') )
-                {
+                if (matchingAliases.includes('md')) {
                     this.drawerMode = 'side';
                     this.drawerOpened = true;
                 }
-                else
-                {
+                else {
                     this.drawerMode = 'over';
                     this.drawerOpened = false;
                 }
@@ -61,67 +56,103 @@ export class DocsComponent implements OnInit, OnDestroy
     private obterMenus(): void {
         this.menuData = [
             {
-                id      : 'inicio',
-                title   : 'Início',
-                type    : 'group',
+                id: 'inicio',
+                title: 'Início',
+                type: 'group',
                 children: [
                     {
-                        id   : 'introducao',
+                        id: 'introducao',
                         title: 'Introdução',
-                        type : 'basic',
-                        link : '/docs/inicio/introducao'
+                        type: 'basic',
+                        link: '/docs/inicio/introducao'
                     },
                     {
-                        id   : 'versoes',
+                        id: 'versoes',
                         title: 'Versões',
-                        type : 'basic',
-                        link : '/docs/inicio/versoes'
+                        type: 'basic',
+                        link: '/docs/inicio/versoes'
                     }
                 ]
             },
             {
-                id      : 'sistema',
-                title   : 'Sistema',
-                type    : 'group',
+                id: 'sistema',
+                title: 'Sistema',
+                type: 'group',
                 children: [
                     {
-                        id   : 'autenticacao',
+                        id: 'autenticacao',
                         title: 'Autenticação',
-                        type : 'basic',
-                        link : '/docs/autenticacao'
+                        type: 'basic',
+                        link: '/docs/autenticacao'
                     },
                     {
-                        id   : 'ordem-servico',
+                        id: 'ordem-servico',
                         title: 'Ordem de Serviço',
-                        type : 'basic',
-                        link : '/docs/ordem-servico'
+                        type: 'basic',
+                        link: '/docs/ordem-servico'
                     },
                     {
-                        id   : 'app-tecnicos',
+                        id: 'app-tecnicos',
                         title: 'App Técnicos',
-                        type : 'basic',
-                        link : '/docs/app-tecnicos'
+                        type: 'basic',
+                        link: '/docs/app-tecnicos'
                     }
                 ]
             },
             {
-                id      : 'desenvolvedor',
-                title   : 'Desenvolvedor',
-                type    : 'group',
+                id: 'desenvolvedor',
+                title: 'Desenvolvedor',
+                type: 'group',
                 children: [
                     {
-                        id   : 'build',
+                        id: 'build',
                         title: 'Build',
-                        type : 'basic',
-                        link : '/docs/build'
+                        type: 'basic',
+                        link: '/docs/build'
+                    }
+                ]
+            },
+            {
+                id: 'documentacao',
+                title: 'Documentação',
+                type: 'group',
+                children: [
+                    {
+                        id: 'docs-intro',
+                        title: 'Introdução',
+                        type: 'basic',
+                        link: '/docs/documentacao/intro'
+                    },
+                    {
+                        id: 'docs-termo-abertura',
+                        title: 'Termo de Abertura',
+                        type: 'basic',
+                        link: '/docs/documentacao/termo-abertura/'
+                    },
+                    {
+                        id: 'docs-escopo',
+                        title: 'Escopo',
+                        type: 'basic',
+                        link: '/docs/documentacao/escopo'
+                    },
+                    {
+                        id: 'docs-custo',
+                        title: 'Custo',
+                        type: 'basic',
+                        link: '/docs/documentacao/custo'
+                    },
+                    {
+                        id: 'docs-tempo',
+                        title: 'Tempo',
+                        type: 'basic',
+                        link: '/docs/documentacao/tempo'
                     }
                 ]
             }
         ];
     }
 
-    ngOnDestroy(): void
-    {
+    ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
---
+++ SAT.FRONT/src/app/modules/main/docs/documentacao/docs-intro/docs-intro.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-docs-intro',
+  templateUrl: './docs-intro.component.html'
+})
+export class DocsIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
